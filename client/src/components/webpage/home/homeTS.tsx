e95948cf27dd4308d997ff429a3faeab44c7bc09
Frank Collins <fwc3rd@gmail.com>
2023-05-16 19:11:31 -0400

    redux test && fix issue #2 [7:11pm]

--- client/src/components/webpage/home/homeTS.tsx
+++ client/src/components/webpage/home/homeTS.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./home.css"
+import {useState, useEffect} from 'react'
+import actionObject from "../../../redux/actions"
+import store from "../../../redux/store"
+// client/src/redux/actions.js
 
 const GraphQLcheck = () => {
   console.log('lemme see');
 }
 
-const test = async () => {
-  let predata = await fetch(`http://localhost:5000/pokemon?query={allbooks{name}}`)
-  let data = await predata.json()
-  console.log('data')
-  console.log(data)
-}
+
 
 let myname:string = "me";
 
@@ -23,10 +22,38 @@ let rumraisin:string = icecream[3];
 let cookiedough:string = icecream[4];
 
 export default function HomeTS () {
+
+  let global_var:any;
+  let setPokemon = actionObject.setPokemon
+  let pokemon;
+
+  const test = async () => {
+    // let query = `{allbooks{name}}`
+    // let predata = await fetch(`http://localhost:5000/fill_cont?query=${query}`)
+    // let data = await predata.json()
+    // console.log('data')
+    // console.log(data)
+    console.log('global_var')
+    console.log(global_var)
+
+    let response = await setPokemon({ payload: "pikachu" }).payload;
+    let pikachu = response.payload; // let pikachu = response.payload.payload;
+    console.log(`I think I'm gonna go just to see ${pikachu}`)
+    
+    
+
+  }
+
+  useEffect( () => {
+    (async() => {
+      global_var = await store.getState()
+
+    })()
+
+  }, [])
+
   return (
     <div id="Page_1">
-      {/* <h1 className={styles.h2}> {cookiedough} </h1> */}
-      {/* <h1> {jackass[0]} </h1> */}
       <button onClick={GraphQLcheck}></button>
       <button onClick={test} id="Btn_Test"> </button>
     </div>
