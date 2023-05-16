f2154dd59f14f2c0489533ecd1ae160d96eb91d9
Frank Collins <fwc3rd@gmail.com>
2023-05-16 19:21:37 -0400

    new function that doesn't take arg/action.payload [7:21pm]

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,27 +1,37 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 
 // Define the initial state
 const initialState = {
   pokemon: 'test-pikachu',
+  water: 'empty'  
 };
 
 // Define the reducer function
-const pokemonReducer = (state = initialState, action) => {
+const fill_cont_reducer = (state = initialState, action) => {
     
   switch (action.type) {
     case 'SET_POKEMON':
       return {
         ...state,
         pokemon: action.payload,
-      };
-    default:
-      return state;
+      };  
+    case 'GET_WATER_BOTTLE':
+        let water = ['aquafina', 'dasani', 'fuji', 'polandspring'];
+        let randomwater = water[Math.floor(Math.random() * water.length)]
+        return {
+          ...state,
+// this doesn't have to be action.payload because we wont be returning the arg. The arg is the payload. Setpokemon needs that arg to set Pokemon
+          water: randomwater
+        };        
+        default:
+          return state;
   }
+  
 };
 
 // Create the store
 
-const store = createStore(pokemonReducer) || "hey guys whatsup";
+const store = createStore(fill_cont_reducer) || "hey guys whatsup";
 export default store;
 
 // clientside use --->
