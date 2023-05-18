d4bf07e7134b22f7d03ec7d83421e78b1cbe9f85
Frank Collins <fwc3rd@gmail.com>
2023-05-17 23:38:02 -0400

    fix issue# 15 obj key reassign & promise [11:37pm]

--- client/src/utility/allurl.ts
+++ client/src/utility/allurl.ts
@@ -1,18 +1,52 @@
-export default async function allurl () {    
+export default async function allurl () { 
+// This func fetches GQL endpoint to get process.env.api. Then returns concatenated url strings data calls appwide.   
     let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,REACT_APP_NODE_ENV}}`)
     // let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,NODE_ENV}}`)
-    let env_data = await pre_envdata.json()  
-    let env = env_data.data.ENV[0].REACT_APP_API
+    let env_data = await pre_envdata.json()
+    let data = env_data.data.ENV
+    
+    let env = data[0].REACT_APP_API
     let dev_and_prod = env.split('***')
 
-    // let env_dev:string = dev_and_prod[0]
-    // let env_prod:string = dev_and_prod[1]
+    console.log('env_data')
+    console.log(env_data)
 
-    // if (NODE)_
+    let env_dev:string = dev_and_prod[0]
+    let env_prod:string = dev_and_prod[1]
 
+    let NODE_ENV = data[0].REACT_APP_NODE_ENV
 
-    // let urlObject = { }
+    let allDBsettingsURL;
+    let API = NODE_ENV === 'deployment' ? env_dev : env_prod
+    let allUsersURL
 
-    return env_data
+    let urlObject = {
+        API: '',    
+        allDBsettingsURL: '',
+        allUsersURL: '',
+        ENVdata: env_data
+    }
+
+    const applyAPI = () => {
+        if (NODE_ENV === 'development') {
+            console.log('NODE_ENV equals deployment')
+            urlObject.API = env_dev;
+            urlObject.allDBsettingsURL = `${API}fill_cont?query={allDBsettings{id,age,height,weight,reminder,activity,star_time,end_time,users_id}}`;
+            urlObject.allUsersURL = `${API}fill_cont?query={allUsers{id,username,password,email,age}}`
+        } else {
+            // let test_query = `{allDBsettings{id,age,height,weight,reminder,activity,start_time,end_time,users_id}}`
+        }
+    }
+    const returnObject = ()=> {return urlObject}
+
+    applyAPI()
+
+    const UrlObjectPromise = new Promise( (resolve, reject) => {
+            resolve(applyAPI())
+    })
+
+    return UrlObjectPromise.then(() => {
+        return urlObject
+    })    
     
 }
