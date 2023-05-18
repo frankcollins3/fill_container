ff5fcda6fc9db7cc2b335fef0dc74c8dee613280
Frank Collins <fwc3rd@gmail.com>
2023-05-17 21:58:45 -0400

    url.split() [9:58pm]
    completely splits it in 2 at the exact string in split('str')
    GraphQL return string with dev && prod concatenated: 
    let serverStringForSplit = `${dev}***${prod}`
    
    split string:
    let dev_and_prod = env.split('***')
    
    1) localhost 2) exampleverceldeployment.url
    (2) ['http://localhost:5000/', 'http://vercelorfriends.com/prod/myapp']
    "http://localhost:5000/"
    "http://vercelorfriends.com/prod/myapp"

--- client/src/utility/allurl.ts
+++ client/src/utility/allurl.ts
@@ -2,13 +2,14 @@ export default async function allurl () {
     let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API}}`)
     let env_data = await pre_envdata.json()  
     let env = env_data.data.ENV[0].REACT_APP_API
+    let dev_and_prod = env.split('***')
+
     console.log('env')
     console.log(env)
-    // /let env = env_data.ENV[0].REACT_APP_API
-    console.log('env_data')
-    console.log(env_data)
-    // console.log(env')
-    // console.log(env)
+    console.log('dev_and_prod')
+    console.log(dev_and_prod)
+
+    // let urlObject = { }
 
     return env_data
     
