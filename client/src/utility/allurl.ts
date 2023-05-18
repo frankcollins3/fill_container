e7c092fb3e1137706e32a79ae243fb29c92bc0ac
Frank Collins <fwc3rd@gmail.com>
2023-05-17 22:16:16 -0400

    include NODE_ENV endpoint in GQLquery [10:16pm]

--- client/src/utility/allurl.ts
+++ client/src/utility/allurl.ts
@@ -1,13 +1,15 @@
 export default async function allurl () {    
-    let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API}}`)
+    let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,REACT_APP_NODE_ENV}}`)
+    // let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,NODE_ENV}}`)
     let env_data = await pre_envdata.json()  
     let env = env_data.data.ENV[0].REACT_APP_API
     let dev_and_prod = env.split('***')
 
-    console.log('env')
-    console.log(env)
-    console.log('dev_and_prod')
-    console.log(dev_and_prod)
+    // let env_dev:string = dev_and_prod[0]
+    // let env_prod:string = dev_and_prod[1]
+
+    // if (NODE)_
+
 
     // let urlObject = { }
 
