73ab4b531dcd6ca60f22a4ce5f386e55acf9a0e2
Frank Collins <fwc3rd@gmail.com>
2023-05-26 14:43:51 -0400

    fix issue #45 [2:43pm]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,13 +17,20 @@ import HomeTS from './components/webpage/home/homeTS'
 import {GoogleLogin, GoogleLogout} from 'react-google-login'
 import {gapi} from 'gapi-script'
 
+// redux / global state management 
+import store from './redux/store'
+import actionObject from './redux/actions'
+
+import allurl from './utility/allurl'
 
 function App() {
 
   // const [googler, setGoogler] = useState(null)
   let env:any;
-  let clientId = ''
+  // let clientId = ''
+  let clientId:string = '';  
   let API:string = ''
+  let GLOBAL_STORE;
   
   const heyguys = {
     hey: 'hi',
@@ -32,92 +39,100 @@ function App() {
   
   const [googleUser, setGoogleUser] = useState<any>({})
   const GoogleUserContext = createContext<any>({})
-  // const [googleUser, setGoogleUser] = useState<object>({})
-  // const GoogleUserContext = createContext<object>({})
 
-  // useEffect to invoke the googleapi. without this the <GoogleLogin> button fails with a 400 error.
   useEffect( () => {
-    (async() => {  
-      // fetch(`http://localhost:5000/fill_cont?query={ENV}`)
-
-// * * CHANGE LOCALHOST FOR PRODUCTION! * * func accesses GraphQL: client/index.js RootQueryType -> ENV { resolve: {process.env}}
-      const fetchAndReassignGlobals = () => {
-        fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,REACT_APP_NODE_ENV,REACT_APP_GOOGLE_ID}}`)
-        .then(async(processenv:any) => {
-          let envdata = await processenv.json()
-          env = envdata.data.ENV[0]
-          clientId = env.REACT_APP_GOOGLE_ID
-          const preAPI = env.REACT_APP_API
-          const splitAPI = preAPI.split("***")
-          const api = env.REACT_APP_NODE_ENV === 'development' ?  splitAPI[0] : splitAPI[1]
-          console.log('splitAPI')
-          console.log(splitAPI)
-  
-          console.log('api')
-          console.log(api)
-          
-          console.log('env')
-          console.log(env)            
-          
-          console.log('clientId')
-          console.log(clientId)
-        })      
-      }
-      // fetchAndReassignGlobals()
+    (async() => {
+      let urlbank = await allurl()
+      console.log('urlbank')
+      console.log(urlbank)
+      GLOBAL_STORE = await store.getState()
+      API = urlbank.API      
       
-      const reassignGlobalGoogleId = () => {
-        fetch(`${API}/fill_cont?query={clientId}`)
-        .then(async(predata:any) => {
-          if (predata) {
-            let data = await predata.json()
-            console.log('data')
-            console.log(data)
-          } else { return } // } else { throw new Error("no please") }        
-        })
-      }
-          
-      function start() {
+      let env = urlbank.ENVdata.data.ENV      
+      clientId = env.GOOGLE_ID
+      console.log('env')
+      console.log(env)
+      // let data = await pre_env.json()
+      // console.log('data')
+      // console.log(data)
+
+            function start() {
         gapi.client.init({
           // clientId: clientId ,
-          clientId: clientId, 
+          clientId: clientId,
+          // clientId: clientId || '569586439008-leid88t18klfhoi2h193rc125aae533l.apps.googleusercontent.com',
+          // clientId: clientId 
           scope: ""
         })
       };
     const loadgoogle = () => { gapi.load('client:auth2', start) }
-      
-      const promises = [
-        fetchAndReassignGlobals,
-        reassignGlobalGoogleId,
-        // loadgoogle
-      ]
 
-      let promise_array:any[] = [fetchAndReassignGlobals()]
-
-      let ServerPromise = new Promise( (resolve, reject) => {
-        resolve(fetchAndReassignGlobals())
-
-        reject(console.log('ServerPromise'))
-      })
-      ServerPromise
-      .then( () => {
-        reassignGlobalGoogleId()
-        loadgoogle()
-      })
-      
     })()
-
-
   }, [])
 
+//   useEffect( () => {
+//     (async() => {  
+//       // fetch(`http://localhost:5000/fill_cont?query={ENV}`)
 
-  const onSuccess = (res:any) => {
-  console.log()
-}
+// // * * CHANGE LOCALHOST FOR PRODUCTION! * * func accesses GraphQL: client/index.js RootQueryType -> ENV { resolve: {process.env}}
+//       const fetchAndReassignGlobals = () => {
+//         fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,REACT_APP_NODE_ENV,REACT_APP_GOOGLE_ID}}`)
+//         .then(async(processenv:any) => {
+//           let envdata = await processenv.json()
+//           env = envdata.data.ENV[0]
+//           clientId = env.REACT_APP_GOOGLE_ID
+//           const preAPI = env.REACT_APP_API
+//           const splitAPI = preAPI.split("***")
+//           const api = env.REACT_APP_NODE_ENV === 'development' ?  splitAPI[0] : splitAPI[1]
+//         })      
+//       }
+//       // fetchAndReassignGlobals()
+      
+//       const reassignGlobalGoogleId = () => {
+//         fetch(`${API}/fill_cont?query={clientId}`)
+//         .then(async(predata:any) => {
+//           if (predata) {
+//             let data = await predata.json()
+//             console.log(data)
+//           } else { return } // } else { throw new Error("no please") }        
+//         })
+//       }
+          
+//       function start() {
+//         gapi.client.init({
+//           // clientId: clientId ,
+//           clientId: clientId || '569586439008-leid88t18klfhoi2h193rc125aae533l.apps.googleusercontent.com',
+//           // clientId: clientId 
+//           scope: ""
+//         })
+//       };
+//     const loadgoogle = () => { gapi.load('client:auth2', start) }
+//     // const promises = [ fetchAndReassignGlobals, reassignGlobalGoogleId, ] let promise_array:any[] = [fetchAndReassignGlobals()]
+      
 
-const onFailure = (res:any) => {
-  // console.log("that didnt work")
-  console.log("hey failure")
-}
+//       let ServerPromise = new Promise( (resolve, reject) => {
+//         resolve(fetchAndReassignGlobals())
+//         reject(console.log('ServerPromise'))
+//       })
+//       ServerPromise
+//       .then( () => {
+//         reassignGlobalGoogleId()
+//         loadgoogle()
+//       })      
+//     })()
+
+
+//   }, [])
+
+
+  const onSuccess = (res:any) =>  {
+     console.log('Promise success') 
+     console.log('res')
+     console.log(res)
+     console.log(res.profileObj)
+    }
+
+const onFailure = (res:any) => { console.log("hey failure") }
 
   if (typeof window === 'undefined') {
     // dotenv.config()
@@ -198,25 +213,23 @@ const test2 = async () => {
   }
 
   return (
-    <GoogleUserContext.Provider value={googleUser} >
+    // <GoogleUserContext.Provider value={googleUser} >
 
     <div className="App">
       <div className="navbar">
-        {/* <h1> were up here for now </h1> */}
         <GoogleLogin  
         onSuccess={onSuccess}
         onFailure={onFailure}
         clientId={clientId}
+        // clientId={GOOGLE_clientId}
         buttonText="text"
         cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
+        // isSignedIn={true}
         />
         <button style={{ backgroundColor: 'maroon', color: 'olive'}} onClick={test}>test</button>
         <button style={{ backgroundColor: 'orange', color: 'maroon'}} onClick={test2}>test2</button>
         {/* <GoogleLogout clientId={clientId}/> */}
         
-
-
         <Navbar />
       </div>
         {renderApp()}
@@ -224,7 +237,7 @@ const test2 = async () => {
         <Credits />
       </div>
     </div>
-    </GoogleUserContext.Provider>
+    // </GoogleUserContext.Provider>
   );
 
 
