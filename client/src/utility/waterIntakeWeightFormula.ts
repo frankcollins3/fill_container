e2e1d689af74e9483dc8ad09c960b62a584154e9
Frank Collins <fwc3rd@gmail.com>
2023-05-28 17:36:00 -0400

    [5:35pm]

--- client/src/utility/waterIntakeWeightFormula.ts
+++ client/src/utility/waterIntakeWeightFormula.ts
@@ -1,10 +1,11 @@
 import React from 'react';
 
 export default async function waterIntakeWeightFormula (weight:number|undefined|null) {
-        if (weight) {
-            let waterIntake:number = weight * (2 / 3);
-            return waterIntake;
-        } else {
-            return 1000
-        }
+        // if (weight) {
+        //     let waterIntake:number = weight * (2 / 3);
+        //     return waterIntake;
+        // } else {
+        //     return 100
+        // }
+        return weight ? weight * (2 / 3) : 100
 }
