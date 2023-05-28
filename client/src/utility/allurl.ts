5978e8e0416a64ee0583867f2d5f82e5f25be476
Frank Collins <fwc3rd@gmail.com>
2023-05-28 19:21:02 -0400

    allow param and allow for string [7:20pm]
    1st lines in block of function parseInt(param) 
    to assert string value upon any function param through if logic

--- client/src/utility/allurl.ts
+++ client/src/utility/allurl.ts
@@ -1,6 +1,15 @@
 // URL bank for APP-API 
-export default async function allurl () { 
+export default async function allurl (users_id:number|undefined|null|string) { 
+    let parsedId:number|undefined;
+    if (typeof users_id === 'string') parsedId = parseInt(users_id)
+    else if (typeof users_id === 'number') parsedId = users_id
+    else { parsedId: undefined }
 // This func fetches GQL endpoint to get process.env.api. Then returns concatenated url strings data calls appwide.   
+
+    // promise based checking of local host or production first, avoiding error crashing the app, and proceeding onto trying the other local or production path string in a fetch
+    // fetch(localhost).then( (!data) => return )
+    // fetch(production)
+
     let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,API,NODE_ENV,GOOGLE_ID}}`)
     // let pre_envdata = await fetch(`http://localhost:5000/fill_cont?query={ENV{DATABASE_URL,REACT_APP_API,NODE_ENV}}`)
     let env_data = await pre_envdata.json()
@@ -32,12 +41,15 @@ export default async function allurl () {
             urlObject.API = API;
             urlObject.allDBsettingsURL = `${API}fill_cont?query={allDBsettings(users_id: 1){id,age,height,weight,reminder,activity,start_time,end_time,users_id}}`;
             urlObject.allUsersURL = `${API}fill_cont?query={allUsers{id,username,password,email,age}}`
-            urlObject.data = `${API}fill_cont?query={data(users_id:1){google_id,access_token,refresh_token,expiry_date,users_id}}`
+            urlObject.data = `${API}fill_cont?query={data(users_id:1){google_id,progress,weekday,date,status,users_id}}`
+            // urlObject.data = `${API}fill_cont?query={data(users_id:${parsedId}){google_id,progress,weekday,date,status,users_id}}`
         } else {
             // let test_query = `{allDBsettings{id,age,height,weight,reminder,activity,start_time,end_time,users_id}}`
         }
     }
-    const returnObject = ()=> {return urlObject}
+    const returnObject = ()=> {
+    return urlObject
+    }
 
     applyAPI()
 
