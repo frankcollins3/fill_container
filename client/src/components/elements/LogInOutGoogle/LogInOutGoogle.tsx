821134b241decacb4bd0cf0382f54b1dde5e4c02
Frank Collins <fwc3rd@gmail.com>
2023-05-31 19:00:07 -0400

    this button will no longer handle anything [6:59pm]
    a box will appear will might look better

--- client/src/components/elements/LogInOutGoogle/LogInOutGoogle.tsx
+++ client/src/components/elements/LogInOutGoogle/LogInOutGoogle.tsx
@@ -1,33 +1,32 @@
 import React from "react"
+import "./LoginOutGoogle.css" 
 
 export default function LogInOutGoogle (user:any) {
 
+    user = user.user
+    // console.log(user.user.GOOGLE_ID)
+
     let InOutGoogleFunction:any;
-
-        if (!user) {
-            console.log("there is no user")
-            //  double confirm if they want to logout. 
-
-            InOutGoogleFunction = () => { 
-                console.log("logout functionality needed")
-            }
-            // logout functionality.
-        } else {
-            if (user.GOOGLE_ID) {
-                // but the user should be able to have a user ID and be able to reject logging into google.
-                InOutGoogleFunction = () => {
-                    console.log("user but no googleID")
-                }
-            } else {
-                // login with user. 
-                InOutGoogleFunction = () => {
-                    console.log("no google login")
-                }
-                return { username: 'testuser', email: 'testemail', password: 'testpassword', age: 'testage' }
-            }
-        }
+        
+        // if (!user) {            
+        //     InOutGoogleFunction = () => { 
+        //         console.log("logout functionality needed")
+        //     }
+        // } else {
+        //     if (user.GOOGLE_ID) {
+        //         InOutGoogleFunction = () => {
+        //             console.log("user but no googleID but the user should be able to have a googleId and be able to reject google login")
+        //         }
+        //     } else {
+        //         // login with user. 
+        //         InOutGoogleFunction = () => {
+        //             console.log("no google login")
+        //         }
+        //         // return { username: 'testuser', email: 'testemail', password: 'testpassword', age: 'testage' }
+        //     }
+        // }
 
         return (
-            <img src="/water_img/exit.png"/>            // could probably do with changing this to door.png
+            <img onClick={InOutGoogleFunction} src="/water_img/exit.png"/>            // could probably do with changing this to door.png
         )
 }
