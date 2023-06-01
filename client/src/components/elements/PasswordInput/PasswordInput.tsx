bfc8269363501ee4f4fc05cb887ea81cc9c0981e
Frank Collins <fwc3rd@gmail.com>
2023-06-01 15:48:34 -0400

    passwordinput [3:48pm]

--- client/src/components/elements/PasswordInput/PasswordInput.tsx
+++ client/src/components/elements/PasswordInput/PasswordInput.tsx
@@ -1,25 +1,53 @@
 import React from 'react'
-import './Password.css'
+import './password.css'
 import {connect, useDispatch} from 'react-redux'
+import $ from 'jquery'
+import attributeJQ from '../../../utility/attributeJQ'
+
+import {SET_PASSWORD} from '../../../redux/actions'
+
  
-function PasswordInput () {
+function PasswordInput (props:any) {
+
+    const { PASSWORD, SET_PASSWORD } = props
+
+    const ghosttext = (event:any) => {
+        let target:any = event.target
+        let jqtarget:any = $(event.target)
+        let targetId:string = event.target.id       
+        if (targetId === 'password') {
+            attributeJQ(target, 'value', PASSWORD)
+        } else {
+            attributeJQ(target, 'value', targetId)         // $(event.target).attr('value', targetId)
+        }
+        // modular function arguments:                  1: target $(event.target)   2: 'value' <input value={}/>        3: targetId: ['username', 'password',]
+    }
+
+    const passwordinputhandler = (event:any) => {
+        let value:string = event.target.value
+        let hashedValue = '*'.repeat(value.length)
+        // let hashedValue = "*".repeat(value.length)
+        $(event.target).attr('value', hashedValue)
+        SET_PASSWORD({payload: hashedValue})
+    }
 
     const renderPasswordInput = () => {
         return (
-            <h1> hey guys </h1>
+<input style={{ color: '#72d3fe', fontSize: '28px'}} value={PASSWORD} onMouseEnter={ghosttext} onChange={passwordinputhandler} id="password" type="text"></input> 
         )
     }
 
     return <div className="PasswordInput-Container"> {renderPasswordInput()} </div>
-}
-
-const mapStateToProps = (state:any) => {
-
-}
-
-const mapDispatchToProps = (dispatch:any) => {
     
 }
+
+const mapStateToProps = (state:any) => ({
+    PASSWORD: state.PASSWORD
+})
+
+const mapDispatchToProps = (dispatch:any) => ({
+    SET_PASSWORD: (action:any) => dispatch(SET_PASSWORD(action))
+})
 
 const ConnectedPasswordInput = connect(mapStateToProps, mapDispatchToProps)(PasswordInput)
 
