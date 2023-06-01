73323e41e9540e8be0949b92c408c44314d2adaa
Frank Collins <fwc3rd@gmail.com>
2023-06-01 17:24:02 -0400

    completely works had to swap ID [5:23pm]

--- client/src/components/elements/EmailInput/EmailInput.tsx
+++ client/src/components/elements/EmailInput/EmailInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./usernameinput.css"
+import "./emailinput.css"
 import {connect, useDispatch} from 'react-redux'
 import $ from 'jquery'
 import attributeJQ from '../../../utility/attributeJQ'
@@ -8,14 +8,11 @@ import inputHandler from '../../../utility/inputHandler'
 
 import {SET_EMAIL_INPUT} from '../../../redux/actions'
 
-
  function EmailInput (props:any) {
     
-    const {USERNAME_INPUT, SET_USERNAME_INPUT} = props
+    const {EMAIL_INPUT, SET_EMAIL_INPUT} = props
     
-    const emailinputhandler = (event:any) => {
-        console.log('event from emailinputhandler')
-        console.log(event)
+    const emailinputhandler = (event:any) => {        
         inputHandler(event, SET_EMAIL_INPUT)
     }
 
@@ -23,17 +20,16 @@ import {SET_EMAIL_INPUT} from '../../../redux/actions'
         let target:any = event.target
         let jqtarget:any = $(event.target)
         let targetId:string = event.target.id       
-        attributeJQ(target, 'value', targetId)         // $(event.target).attr('value', targetId)
-        
+        attributeJQ(target, 'value', targetId)         // $(event.target).attr('value', targetId)        
     }
 
-    const renderUsernameInput = () => {
+    const renderEmailInput = () => {
         return (
-        <input value={USERNAME_INPUT} onMouseEnter={ghosttext} onChange={emailinputhandler} id="username" type="text"></input> 
+        <input value={EMAIL_INPUT} onMouseEnter={ghosttext} onChange={emailinputhandler} id="email" type="text"></input> 
         )
     }
 
-    return (<div className="UsernameInput-container">{renderUsernameInput()} </div>)
+    return (<div className="EmailInput-Container">{renderEmailInput()} </div>)
 
 }
 
@@ -42,7 +38,7 @@ const mapStateToProps = (state:any) => ({
 })
 
 const mapDispatchToProps =(dispatch:any) => ({
-    SET_USERNAME_INPUT: (action:any) => dispatch(SET_EMAIL_INPUT(action))
+    SET_EMAIL_INPUT: (action:any) => dispatch(SET_EMAIL_INPUT(action))
 })
 
 const ConnectedEmailInput = connect(mapStateToProps, mapDispatchToProps)(EmailInput)
