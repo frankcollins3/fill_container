bf1e1ad108147d04e44e4edaeb4d5ab4aded4e6d
Frank Collins <fwc3rd@gmail.com>
2023-05-31 21:44:55 -0400

    updated redux reducer [9:44pm]

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,62 +1,129 @@
-import { combineReducers } from 'redux';
+// const [hydroData, setHydroData] = useState();              // '/data'
+// const [hydroIntake, setHydroIntake] = useState();
+// const [hydroSchedule, setHydroSchedule] = useState([]);
+// const [hydroSettings, setHydroSettings] = useState();
+// const [reload, setReload] = useState();
+// const [date, setDate] = useState();
+// const [loading, setLoading] = useState(true);
+// const [error, setError] = useState();
 
+
+// initial state for redux 
 const initialState = {
-  pokemon: 'test-pikachu',
-  water: 'empty',
-  API_URL: '',
-  settings: false,
-  LOGIN_TYPE: 'TEST_LOGIN_TYPE',
-  ENV: {
-    DATABASE_URL: 'TEST_URL',
-    API: 'TEST_API',
-    NODE_ENV: 'TEST_NODE_ENV',
-    GOOGLE_ID: 'TEST_GOOGLE_ID',
-  },
-  USER: {
-    USERNAME: 'testusername',
-    EMAIL: 'testemail',
-    PASSWORD: 'testpassword',
-    AGE: 'testage',
-    GOOGLE_ID: 'noneyet',
-  },
-};
+    HYDRO_SETTINGS: false,
 
-const rootReducer = (state = initialState, action) => {
+    HYDRO_DATA: {
+      id: 0,
+      google_id: '',
+      date: 1992-11-21,
+      progress: 0,
+      weekday: 'noneday',
+      status: {},
+      users_id: 0
+    },
+    LOG_IN_OUT_TYPE: 'LOGIN',
+    LOGIN_SIGNUP_BTN: false
+  };
+
+  const rootReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'SET_POKEMON':
-          return {
-            ...state,
-            pokemon: action.payload,
-          };  
-        case 'GET_WATER_BOTTLE':
-            let water = ['aquafina', 'dasani', 'fuji', 'polandspring'];
-            let randomwater = water[Math.floor(Math.random() * water.length)]
-            return {
-              ...state,
-    // this doesn't have to be action.payload because we wont be returning the arg. The arg is the payload. Setpokemon needs that arg to set Pokemon
-              water: randomwater
-            };      
-        case "SET_API_URL":        
-            return {
-              ...state,
-              API_URL: action.payload,
-            }
-        case "TOGGLE_SETTINGS":
-            return {
-              ...state,
-              settings: action.payload ? action.payload === false ? true : false : true 
-            }
-        case "SET_LOGIN_TYPE":
-            return {
-              ...state,
-              LOGIN_TYPE: action.payload
-            }
-            default:
-              return state;
-      }
-};
+      case 'TOGGLE_HYDRO_SETTINGS':
+        return {
+          ...state,
+          // HYDRO_SETTINGS: state.HYDRO_SETTINGS === false ? true : false   
+          HYDRO_SETTINGS: !state.HYDRO_SETTINGS,     // this is saying set the boolean to the value that it isn't which would toggle it true/false.
+        };
+      case 'SET_HYDRO_DATA':
+        return {
+          ...state,
+          HYDRO_DATA: { 
+            id: action.payload.id, google_id: action.payload.google_id, date: action.payload.date, progress: action.payload.progress,
+            weekday: action.payload.weekday, status: action.payload.status, users_id: action.payload.users_id
+          }
+        }
+      case 'SET_LOG_IN_OUT_TYPE':
+        return {
+          ...state,
+          LOG_IN_OUT_TYPE: action.payload
+          // LOG_IN_OUT_TYPE: !action.payload ? "LOGOUT" : action.payload === "GOOGLE" ? "GOOGLE" : action.payload === "LOGOUT" ? "LOGOUT" : "LOGIN"
+        }
+      case 'TOGGLE_LOGIN_SIGNUP_BTN':
+        return {
+          ...state,
+          LOGIN_SIGNUP_BTN: state.LOGIN_SIGNUP_BTN === false ? true : false 
+          // LOGIN_SIGNUP_BTN: !state.LOGIN_SIGNUP_BTN
+        }
 
-export default combineReducers({
-  // Add your reducers here if you have multiple reducers
-  rootReducer,
-});
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
+  
+
+
+  // hydroData                  // is the entire response of the data from the './data' route.     This is the same data that is associated to the user. 
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const response = await WAPPRequest('/data', {
+  //       method: 'GET',
+  //     }).catch(() => {
+  //       setError(true);
+  //       return null;
+  //     });
+
+  //     if (response) {
+  //       setHydroData(response);
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
+
+// hydroIntake:             body weight based calculation for a value that apparently represents how much water the currentUser must drink.
+  // setHydroIntake(
+  //   response.settings  
+  //     ? Math.ceil(response.settings.weight * (2 / 3))      
+  //     : 100
+  // );
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
+//  hydroSchedule           [ username, email, password, age, settings, schedule ]      // the schedule is the same data in the schedule column for the user table.
+//  setHydroSchedule(response.schedule);
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
+
+//  hydroSettings          [username, email, password, age, settings, schedule ]        // settings is the same as the data in the user table column for postgres.
+//  setHydroSettings(response.settings);
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+
+
+// [date, setDate] -----> 
+
+// 1: const date = new Date();        //      this date is not the same date as the state date. this date is a locally declared date within the useEffect( () => ) from App.jsx WAPP
+// setDate(
+  //   `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()}`
+  // );
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+  
+  
+
+  // [loading, setLoading] = (true)
+  // setLoading(false);               // this is done in the opening useEffect that deals with the main data.
+
+  // if (loading) { <spinner/> }            // the loading is set to true until the data hits.
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+  
+
+
+  // const [reload, setReload] = useState();
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
