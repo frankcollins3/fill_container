68cff4ba4409ba63913c9333a090de468c6d7b22
Frank Collins <fwc3rd@gmail.com>
2023-06-02 19:40:44 -0400

    click the input and the values disappear [7:40pm]

--- client/src/components/elements/PasswordInput/PasswordInput.tsx
+++ client/src/components/elements/PasswordInput/PasswordInput.tsx
@@ -1,54 +1,74 @@
 import React from 'react'
-import './password.css'
+import "./password.css"
 import {connect, useDispatch} from 'react-redux'
 import $ from 'jquery'
 import attributeJQ from '../../../utility/attributeJQ'
+import ghostText from '../../../utility/ghostText'
+import inputHandler from '../../../utility/inputHandler'
+import inputFocusToggleRedux from '../../../utility/inputFocusToggleRedux'
 
-import {SET_PASSWORD} from '../../../redux/actions'
+import ConnectedSignupLoginChecker from '../../../components/elements/SignupLoginChecker'
 
- 
-function PasswordInput (props:any) {
+import {SET_PASSWORD_INPUT, SET_DUMMY_PASSWORD_INPUT, TOGGLE_INPUT_FOCUS} from '../../../redux/actions'
 
-    const { PASSWORD, SET_PASSWORD } = props
+function AgeInput (props:any) {
+    //  function UsernameInput (props:any) {
+
+    let { 
+         TOGGLE_INPUT_FOCUS, PASSWORD_INPUT, DUMMY_PASSWORD_INPUT,
+         SET_PASSWORD_INPUT, SET_DUMMY_PASSWORD_INPUT
+        } = props;
+
+        // const {USERNAME_INPUT, SET_USERNAME_INPUT} = props
+    
+    const passwordinputhandler= (event:any) => {
+        console.log('event from usernameinputhandler')
+        console.log(event)
+        inputHandler(event, SET_PASSWORD_INPUT)
+    }
 
     const ghosttext = (event:any) => {
         let target:any = event.target
         let jqtarget:any = $(event.target)
         let targetId:string = event.target.id       
         if (targetId === 'password') {
-            attributeJQ(target, 'value', PASSWORD)
+            // attributeJQ(target, 'value', PASSWORD)
+            
         } else {
             attributeJQ(target, 'value', targetId)         // $(event.target).attr('value', targetId)
         }
         // modular function arguments:                  1: target $(event.target)   2: 'value' <input value={}/>        3: targetId: ['username', 'password',]
     }
+    
+    const inputfocus = async () => {
+         SET_PASSWORD_INPUT( { payload: ''})
+         TOGGLE_INPUT_FOCUS( { payload: 'password'} ) 
+        }
 
-    const passwordinputhandler = (event:any) => {
-        let value:string = event.target.value
-        let hashedValue = '*'.repeat(value.length)
-        // let hashedValue = "*".repeat(value.length)
-        $(event.target).attr('value', hashedValue)
-        SET_PASSWORD({payload: hashedValue})
-    }
-
+                                                                                    
     const renderPasswordInput = () => {
         return (
-<input style={{ color: '#72d3fe', fontSize: '28px'}} value={PASSWORD} onMouseEnter={ghosttext} onChange={passwordinputhandler} id="password" type="text"></input> 
+<input style={{ color: '#72d3fe', fontSize: '20px'}} onFocus={inputfocus} value={PASSWORD_INPUT} onMouseEnter={ghosttext} onChange={passwordinputhandler} id="password" type="text"></input> 
         )
     }
 
-    return <div className="PasswordInput-Container"> {renderPasswordInput()} </div>
-    
+    return (<div className="UsernameInput-container">{renderPasswordInput()} </div>)
+
 }
 
 const mapStateToProps = (state:any) => ({
-    PASSWORD: state.PASSWORD
+    PASSWORD_INPUT: state.PASSWORD_INPUT,
+    DUMMY_PASSWORD_INPUT: state.DUMMY_PASSWORD_INPUT,
+    INPUT_FOCUS: state.INPUT_FOCUS,
+    // ALL_USERS: state.ALL_USERS
 })
 
-const mapDispatchToProps = (dispatch:any) => ({
-    SET_PASSWORD: (action:any) => dispatch(SET_PASSWORD(action))
+const mapDispatchToProps =(dispatch:any) => ({
+    SET_PASSWORD_INPUT: (action:any) => dispatch(SET_PASSWORD_INPUT(action)),
+    SET_DUMMY_PASSWORD_INPUT: (action:any) => dispatch(SET_DUMMY_PASSWORD_INPUT(action)),
+    TOGGLE_INPUT_FOCUS: (action:any) => dispatch(TOGGLE_INPUT_FOCUS(action))
 })
 
-const ConnectedPasswordInput = connect(mapStateToProps, mapDispatchToProps)(PasswordInput)
+const ConnectedUsernameInput = connect(mapStateToProps, mapDispatchToProps)(AgeInput)
 
-export default ConnectedPasswordInput
+export default ConnectedUsernameInput
