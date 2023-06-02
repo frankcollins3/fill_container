a7ea23425cebfc081d321ee17669988e801bad7f
Frank Collins <fwc3rd@gmail.com>
2023-06-02 01:17:23 -0400

    fix issue #83 setCursor() works on some not all [1:16am]

--- client/src/utility/setCursor.ts
+++ client/src/utility/setCursor.ts
@@ -3,9 +3,7 @@ import CSS from './CSS'
 export default async function setCursor () {
     if (typeof window !== 'undefined') {
         let eventassertions = [ {property: 'cursor', value: `normal`}]
-        CSS($('*'), 'cursor', `url('/water_img/mouse_droplet.png')`)   
-        // EVENT($('*'), 'mouseenter', eventassertions) 
+        CSS($('*'), 'cursor', `url('/water_img/mouse_droplet.png'), auto`)           
           $('*').on('mouseenter', (event:any) => { CSS($(event.target), 'cursor', 'normal') })
-        // let eventassertions = [CSS(target, 'cursor', normal)]
       }
 }
