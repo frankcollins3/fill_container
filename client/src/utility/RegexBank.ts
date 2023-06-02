139d9534401598dbf240836ffe9e96be405ac770
Frank Collins <fwc3rd@gmail.com>
2023-06-02 10:55:16 -0400

    hopefully fix issue #86 [10:54am]
    if you remove .replace() and return just the regex expression
    in my case, dont try to handle the second arg to .replace()
    if there is no .replace()
    
    str.replace(regex, '')
    ^ this has to be done clientside ^ 
    or the replace.() has to be intact with the regex
    
    at time of running the statements

--- client/src/utility/RegexBank.ts
+++ client/src/utility/RegexBank.ts
@@ -1,7 +1,21 @@
-export default function RegexBank (str:string, regex:string) {
+export default function RegexBank (str:string, regex:string) {    
     const regexobject = {
-        stringAfterPeriod: str.replace(/^.*\.(.*)$/, '$1'),
-        returnLettersAthruZ: str.replace(/[a\-z]/g, ''),
-        returnNumbers: str.replace(/[0\-9]/g, '')
+        stringAfterPeriod: /^.*\.(.*)$/,
+        returnLettersAthruZ: /[a\-z]/g,
+        returnNumbers: /[0\-9]/g
+    }
+    let regobj = regexobject
+    
+    if (regex === "stringAfterPeriod") {
+        return regobj.stringAfterPeriod        
+    }
+    if (regex === "returnNumbers") {
+        return regobj.returnNumbers
     }
 }
+
+// const regexobject = {
+//     stringAfterPeriod: str.replace(/^.*\.(.*)$/, '$1'),
+//     returnLettersAthruZ: str.replace(/[a\-z]/g, ''),
+//     returnNumbers: str.replace(/[0\-9]/g, '')
+// }
