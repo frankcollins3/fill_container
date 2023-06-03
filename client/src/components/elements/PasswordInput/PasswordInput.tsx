3759eb487f9bbae9603c62e59b443cbb74afe6d8
Frank Collins <fwc3rd@gmail.com>
2023-06-03 17:31:35 -0400

    avoid issue by typing state [5:31pm]
    almost posted issue:
    
    1: have to type state as such with <any>
         const [inputState, setInputState] = useState<any>()
    
    2:
    const [inputState, setInputState] = useState( [] )
    
    // before, i had state set up as empty array.
    
    
    const passwordinputhandler = async (evt: React.ChangeEvent<HTMLInputElement>) => {
                let target = evt.target
      let value:any = evt.target.value // !no type as string for loop
                for (const char of value) {                
                    let c = value[char]
                    setInputState([inputState, c])
                }
    
    
    2.2:
                    setInputState([inputState, c])
    
    problem without typing state as <any> with no defined []
    is that this method of setState returns an array
    "never cant be set on type any"

--- client/src/components/elements/PasswordInput/PasswordInput.tsx
+++ client/src/components/elements/PasswordInput/PasswordInput.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./password.css"
 import {connect, useDispatch} from 'react-redux'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import $ from 'jquery'
 import attributeJQ from '../../../utility/attributeJQ'
 import ghostText from '../../../utility/ghostText'
 import inputHandler from '../../../utility/inputHandler'
 import inputFocusToggleRedux from '../../../utility/inputFocusToggleRedux'
 import halfAssHash from '../../../utility/halfAssHash'
+import duplicateString from '../../../utility/duplicateString'
+
 
 import ConnectedSignupLoginChecker from '../../../components/elements/SignupLoginChecker'
 
@@ -15,9 +17,9 @@ import {SET_PASSWORD_INPUT, SET_DUMMY_PASSWORD_INPUT, TOGGLE_INPUT_FOCUS} from '
 
 function PasswordInput (props:any) {
 
-    const [dummyPassword, setDummyPassword] = useState('')
-    const [inputFocus, setInputFocus] = useState(false)
-    //  function UsernameInput (props:any) {
+     const [passwordState, setPasswordState] = useState("")
+     const [dummyState, setDummyState] = useState("")
+     const [inputState, setInputState] = useState<any>()
 
     let { 
          TOGGLE_INPUT_FOCUS, PASSWORD_INPUT, DUMMY_PASSWORD_INPUT,
@@ -26,61 +28,42 @@ function PasswordInput (props:any) {
 
         // const {USERNAME_INPUT, SET_USERNAME_INPUT} = props
 
-        const inputhandlerCB = (inputHandlerObj: any) => {
-            console.log('inputHandlerObj')
-            console.log(inputHandlerObj)
-            // let value: string = inputHandlerObj.target.value;
-            setDummyPassword(inputHandlerObj.target.value)    
+        useEffect( () => {
+            setDummyState("*".repeat(dummyState.length))
+        }, [dummyState])
+        
+        const passwordinputhandler = async (evt: React.ChangeEvent<HTMLInputElement>) => {
+            let target = evt.target
+            let value:any = evt.target.value  // cant use string because we'll be looping over it 
+            
+            for (const char of value) {                
+                let c = value[char]
+                setInputState([inputState, c])
+            }
+            // let duplicateValue:string = await duplicateString(evt.target.value)
+            // let value;
+            // let dummyArray = [duplicateValue, value]
+            // value = dummyArray[0]
+            // console.log('duplicateValue')
+            // console.log(duplicateValue)
+            // SET_PASSWORD_INPUT( {payload: evt.target.value })
+        };
+        
+        const passwordinputhandler2 = async (event: React.ChangeEvent<HTMLInputElement>) => {
 
-            console.log('inputhandlerCB FUNCTION');
-            console.log('dummyPassword')
-            console.log(dummyPassword)
+                // let value:string = event.target.value.toUpperCase()
+                // console.log('PASSWORD_INPUT')
+                // console.log(PASSWORD_INPUT)
+
+                // console.log('DUMMY_PASSWORD_INPUT')
+                // console.log(DUMMY_PASSWORD_INPUT)
+            
+                // const uniqueChars = new Set(dummyState.split(''));
+                // uniqueChars.add(value);
+                // setDummyState(value);
+            //   SET_DUMMY_PASSWORD_INPUT({payload: value })
           };
 
-          
-          const passwordinputhandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-            // Access event properties here
-          };
-          
-          const passwordinputhandler2 = (event: React.ChangeEvent<HTMLInputElement>) => {
-            // Access event properties here
-          };
-
-          const passwordinputhandler3 = () => {            
-            let preinput = $('#password')
-            // $('#password')[0].attributes[2].nodeValue
-            let inputelem = preinput
-            let elemValue:string|undefined|null = preinput[0].attributes[2].nodeValue
-            console.log('inputelem')
-            console.log(inputelem)
-
-            console.log('elemValue')
-            console.log(elemValue)
-            // let elemValue:string = inputelem
-
-            // SET_PASSWORD_INPUT({payload: onChangeValue})           
-          }
-
-        //   const prepasswordinputhandler = (event:any) => {
-        //         setDummyPassword(event.target.value)
-        //         let eventValueLength:number = event.target.value.length
-        //         console.log('event.target.value')
-        //         const clone_1 = {...event}
-        //         const clone_2 = {...event} 
-        //         let clone2value:string = clone_2.target.value    
-        //         console.log('clone2value')
-        //         console.log(clone2value)
-
-        //         // let hashedCharacter = halfAssHash("*", clone_1.target.value)
-        //         // console.log('hashedCharacter')
-        //         // console.log(hashedCharacter)
-        //         SET_PASSWORD_INPUT({payload: clone_1.target.value })
-        //         SET_DUMMY_PASSWORD_INPUT({ payload: clone_2.target.value })
-        //         // SET_DUMMY_PASSWORD_INPUT({payload: "*".repeat(clone2value.length)})
-        //         console.log('PASSWORD_INPUT')
-        //         console.log(PASSWORD_INPUT)                                 
-        //   }
-          
 
     const ghosttext = (event:any) => {
         let target:any = event.target
@@ -104,16 +87,15 @@ function PasswordInput (props:any) {
     const renderPasswordInput = () => {
         return (
         <>
-<input id="password" type="text" style={{ color: '#72d3fe', fontSize: '20px'}} onFocus={inputfocus} value={DUMMY_PASSWORD_INPUT} onMouseEnter={ghosttext}
-// onChange={prepasswordinputhandler} 
+<input id="password" type="text" style={{ color: '#72d3fe', fontSize: '20px'}} onFocus={inputfocus} value={inputState} onMouseEnter={ghosttext} // "*".repeat(DUMMY_PASSWORD_INPUT.length)
+// <input id="password" type="text" style={{ color: '#72d3fe', fontSize: '20px'}} onFocus={inputfocus} value={DUMMY_PASSWORD_INPUT} onMouseEnter={ghosttext} // "*".repeat(DUMMY_PASSWORD_INPUT.length)
 onChange={(event) => {
     passwordinputhandler(event);
     passwordinputhandler2(event);
 }}>
 
 </input> 
-<p style={{ textAlign: 'center' }}> "PASSWORD_INPUT": {PASSWORD_INPUT || 'no pw'} </p>
-<p style={{ textAlign: 'center' }}> "dummyPassword " {dummyPassword || 'no pw' } </p>
+<p style={{ textAlign: 'center' }}> : {PASSWORD_INPUT || 'no pw'} </p>
     </>
         )
     }
