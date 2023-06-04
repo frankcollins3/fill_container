c4e0669e14a90f639683cb606a0811e08e01d38a
Frank Collins <fwc3rd@gmail.com>
2023-06-03 21:53:48 -0400

    regex for .test() [9:53pm]

--- client/src/utility/RegexBank.ts
+++ client/src/utility/RegexBank.ts
@@ -1,17 +1,21 @@
-export default function RegexBank (str:string, regex:string) {    
+export default function RegexBank () {    
     const regexobject = {
         stringAfterPeriod: /^.*\.(.*)$/,
         returnLettersAthruZ: /[a\-z]/g,
-        returnNumbers: /[0\-9]/g
+        returnNumbers: /[0\-9]/g,
+
+        hasCaps: /[A-Z]/g,
+        hasNums: /[A-Z]/g,
     }
-    let regobj = regexobject
-    
-    if (regex === "stringAfterPeriod") {
-        return regobj.stringAfterPeriod        
-    }
-    if (regex === "returnNumbers") {
-        return regobj.returnNumbers
-    }
+    return regexobject
+    // let regobj = regexobject
+
+    // if (regex === "stringAfterPeriod") {
+    //     return regobj.stringAfterPeriod        
+    // }
+    // if (regex === "returnNumbers") {
+    //     return regobj.returnNumbers
+    // }
 }
 
 // const regexobject = {
