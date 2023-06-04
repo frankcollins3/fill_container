7fa99101aac1b96d161ee5d0b015be928e4cb21a
Frank Collins <fwc3rd@gmail.com>
2023-06-04 01:20:22 -0400

    CURRENT_USER with redux working [1:20am]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import React from "react"
 import {useState, useEffect, useContext, createContext } from 'react';
-import { connect, Provider } from "react-redux"
+import { connect, Provider, useDispatch } from "react-redux"
+
 import $ from 'jquery'
 import dotenv from "dotenv"
 
@@ -10,14 +11,15 @@ import dotenv from "dotenv"
 import WaterRequest from './utility/WaterRequest'
 import CSS from './utility/CSS'
 import EVENT from './utility/EVENT'
-import allurl from './utility/allurl'
+import allDBurl from './utility/fetch/allDBurl'
 import setCursor from './utility/setCursor'
+import waterIntakeWeightFormula from './utility/waterIntakeWeightFormula'
 
 // * components from src/components *
 import Navbar from './components/elements/Navbar'
 import Dashboard from './components/elements/Dashboard/Dashboard'
 import Credits from './components/elements/Credits/Credits'
-import Settings from './components/elements/Settings/Settings'
+import ConnectedLogInOutGoogle from './components/elements/LogInOutGoogle/LogInOutGoogle'
 import HomeTS from './components/webpage/home/homeTS'
 
 // <GoogleLogin> and googleAPI components and variables.
@@ -29,10 +31,11 @@ import store from './redux/store'
 import { TOGGLE_HYDRO_SETTINGS, SET_LOG_IN_OUT_TYPE } from './redux/actions'
 
 function App( props:any ) {
+  const dispatch = useDispatch()
   setCursor()
 
   const { 
-    HYDRO_SETTINGS, LOG_IN_OUT_TYPE,            // state from mapStateToProps above the export app statement.
+    HYDRO_SETTINGS, LOG_IN_OUT_TYPE, CURRENT_USER,           // state from mapStateToProps above the export app statement.
     TOGGLE_HYDRO_SETTINGS, SET_LOG_IN_OUT_TYPE      // actions from mapDispatchToProps bottom of App.tsx
   } = props    // object destructuring props haven't done this before.
 
@@ -49,7 +52,7 @@ function App( props:any ) {
 
   useEffect( () => {
     (async() => {
-      urlbank = await allurl()
+      urlbank = await allDBurl()
       // let {HYDRO_DATA, HYDRO_SETTINGS } = await store.getState()
 
       API = urlbank.API      
@@ -71,21 +74,26 @@ function App( props:any ) {
   const onSuccess = (res:any) =>  { console.log(res.profileObj) }
   const onFailure = (res:any) => { console.log("hey failure") }
   
-  const test = async () => {
-    // await TOGGLE_HYDRO_SETTINGS()
-    let mydata = await fetch(`${urlbank.data}`)
-    // let mydata = await fetch(`${API}fill_cont?query={data(users_id:1){google_id,access_token,refresh_token,expiry_date,users_id}}`)
-    let data = await mydata.json()
-    console.log('data')
-    console.log(data)
-    // `${API}fill_cont?query={allDBsettings(users_id: 1){id,age,height,weight,reminder,activity,start_time,end_time,users_id}}`;
+  const test = async () => {    
+    let possible = ["LOGIN", "LOGOUT", "GOOGLE"]
+    let random_possible = possible[Math.floor(Math.random()*possible.length)]
+    let action = { payload: random_possible };
+    console.log('action.payload');
+    console.log(action.payload);
+    // dispatch(SET_LOG_IN_OUT_TYPE(action));
+    dispatch(SET_LOG_IN_OUT_TYPE(action))
+    // dispatch(SET_LOG_IN_OUT_TYPE(action))
   };
-
+  
   const test2 = async () => {
-    // let allDBsettings:string = urlbank.allDBsettingsURL
-    // let h20 = await WaterRequest(allDBsettings, { headers: 'headers' })
-    // console.log(await store.getState())   
-    console.log(urlbank) 
+    let myIntake:number = await waterIntakeWeightFormula('222');
+    console.log('myIntake')
+    console.log(myIntake)
+  }
+
+  const usertest = () => {
+    console.log('CURRENT_USER from app.tsx')
+    console.log(CURRENT_USER)
   }
 
   const renderApp = () => {
@@ -95,7 +103,9 @@ function App( props:any ) {
     <Router>
     <Routes>
     <Route path={'/'} element={ < HomeTS /> } />
-    <Route path={'/settings'} element={ < Settings /> } />
+    {/* <Route path={'/settings'} element={ < Settings /> } /> */}
+    {/*   settings needs redux state. [ SETTINGS_DISPLAY | TOGGLE_SETTINGS_DISPLAY ]   */}
+    <Route path={'/loginoutgoogle'} element={ <ConnectedLogInOutGoogle/> } />
     <Route path={'/dashboard'} element={ < Dashboard /> } />
     </Routes>
     </Router>  
@@ -106,22 +116,20 @@ function App( props:any ) {
   return (
     // <GoogleUserContext.Provider value={googleUser} >
     <div className="App">
-      <div className="navbar">
-        
-        <GoogleLogin  
-        onSuccess={onSuccess} onFailure={onFailure} buttonText="text" cookiePolicy={'single_host_origin'} isSignedIn={true}
-        clientId='569586439008-leid88t18klfhoi2h193rc125aae533l.apps.googleusercontent.com'
-        // clientId={clientId.length > 3 ? clientId : ''}        
-        />
-        <button style={{ backgroundColor: 'maroon', color: 'olive'}} onClick={test}>test</button>
-        <button style={{ backgroundColor: 'orange', color: 'maroon'}} onClick={test2}>test2</button>
-        
+      <div className="navbar">      
+        {/* <button style={{ backgroundColor: 'maroon', color: 'olive'}} onClick={test}>test</button>
+        <button style={{ backgroundColor: 'orange', color: 'maroon'}} onClick={test2}>test2</button> */}
         <Navbar />
+        <h1 className="lifewater"> Water is Life</h1>
       </div>
         {renderApp()}
       <div className="credits">
+        <h1 className="lifewater"> {CURRENT_USER.username ? `${CURRENT_USER.username} is Water` : "Life is Water" } </h1>
+        {/* <h1 className="lifewater"> Life is Water </h1> */}
         <Credits />
-      <p style={{ textAlign: 'center' }}> settings: { HYDRO_SETTINGS ? 'on' : 'off' } </p>
+      {/* <p style={{ textAlign: 'center' }}> LOG_IN_OUT_TYPE: { LOG_IN_OUT_TYPE  } </p> */}
+      {/* <p style={{ textAlign: 'center' }}> LOG_IN_OUT_TYPE: { LOG_IN_OUT_TYPE === 'login' ? 'login' : 'logout' } </p> */}
+      {/* <p style={{ textAlign: 'center' }}> settings: { HYDRO_SETTINGS ? 'on' : 'off' } </p> */}
       </div>
     </div>
     // </GoogleUserContext.Provider>
@@ -137,13 +145,15 @@ const mapStateToProps = (state:any) => ({
     // regular app props
     LOG_IN_OUT_TYPE: state.LOG_IN_OUT_TYPE,
     HYDRO_SETTINGS: state.HYDRO_SETTINGS,
+
+    CURRENT_USER: state.CURRENT_USER
 });
 
 // global redux actions. these are the state-mutating actions being mapped to props
-const mapDispatchToProps = (dispatch:any) => ({
+const mapDispatchToProps = (dispatch: any) => ({
   TOGGLE_HYDRO_SETTINGS: () => dispatch(TOGGLE_HYDRO_SETTINGS()),
-  SET_LOG_IN_OUT_TYPE: () => dispatch(SET_LOG_IN_OUT_TYPE()),
-})
+  SET_LOG_IN_OUT_TYPE: (action: any) => dispatch(SET_LOG_IN_OUT_TYPE(action))
+});
 
 const ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App);
 
