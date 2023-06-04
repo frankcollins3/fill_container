d891c2d2ad198aad005c92202cc9e61efe8fb8b7
Frank Collins <fwc3rd@gmail.com>
2023-06-04 16:33:57 -0400

    add param which takes object (elem to assert cursor style over) [4:33pm]

--- client/src/utility/setCursor.ts
+++ client/src/utility/setCursor.ts
@@ -1,9 +1,9 @@
 import $ from 'jquery'
 import CSS from './CSS'
-export default async function setCursor () {
+export default async function setCursor (elem:any) {
     if (typeof window !== 'undefined') {
         let eventassertions = [ {property: 'cursor', value: `normal`}]
-        CSS($('*'), 'cursor', `url('/water_img/mouse_droplet.png'), auto`)           
+        CSS($(elem), 'cursor', `url('/water_img/mouse_droplet.png'), auto`)           
           $('*').on('mouseenter', (event:any) => { CSS($('*'), 'cursor', 'normal') })
         //   $('*').on('mouseenter', (event:any) => { CSS($(event.target), 'cursor', 'normal') })
       }
