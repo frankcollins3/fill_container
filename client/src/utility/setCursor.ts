b10215003f6ca08b477ec195871d3f9c303bbab3
Frank Collins <fwc3rd@gmail.com>
2023-06-04 12:17:29 -0400

    update utility scripts cursor function [12:17pm]

--- client/src/utility/setCursor.ts
+++ client/src/utility/setCursor.ts
@@ -4,6 +4,7 @@ export default async function setCursor () {
     if (typeof window !== 'undefined') {
         let eventassertions = [ {property: 'cursor', value: `normal`}]
         CSS($('*'), 'cursor', `url('/water_img/mouse_droplet.png'), auto`)           
-          $('*').on('mouseenter', (event:any) => { CSS($(event.target), 'cursor', 'normal') })
+          $('*').on('mouseenter', (event:any) => { CSS($('*'), 'cursor', 'normal') })
+        //   $('*').on('mouseenter', (event:any) => { CSS($(event.target), 'cursor', 'normal') })
       }
-}
+    }
