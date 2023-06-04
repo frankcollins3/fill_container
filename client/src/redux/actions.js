b02b417068267f48baefa3ed32393d1f2ca83cd9
Frank Collins <fwc3rd@gmail.com>
2023-06-04 10:29:35 -0400

    /redux/actions-> new state && new format [10:28am]
    comment structure:
    1: file in which redux state is changed or used
    2: the function name thats doing that
    3: what happens as output after that function invocation
    
    1: 
    //        /Navbar.tsx ------->      
    
    2: 
    const settingsclick = () => { } else { TOGGLE_HYDRO_SETTINGS() } }   ----> gear.png.click()   TOGGLE_HYDRO_SETTINGS() 
    
    3: 
    make the settings sidebar appear or navigate page to do so

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,14 +1,18 @@
+//        /Navbar.tsx ------->      const settingsclick = () => { } else { TOGGLE_HYDRO_SETTINGS() } }   ----> gear.png.click()   TOGGLE_HYDRO_SETTINGS() make the settings sidebar appear or navigate page to do so
 export const TOGGLE_HYDRO_SETTINGS = () => { return { type: 'TOGGLE_HYDRO_SETTINGS', } };
 
-export const SET_LOG_IN_OUT_TYPE = (action) => {
-    return {
-      type: "SET_LOG_IN_OUT_TYPE",
-      payload: action.payload
-    }
-};
+// 
+// export const SET_LOG_IN_OUT_TYPE = (action) => {
+//     return {
+//       type: "SET_LOG_IN_OUT_TYPE",
+//       payload: action.payload
+//     }
+// };
 
+//      /LogInOutGoogle.tsx   ---------->         const showHideLoginSignupBtn = () => {          TOGGLE_LOGIN_SIGNUP_BTN()           ------>     <img onClick={showHideLoginSignupBtn}
 export const TOGGLE_LOGIN_SIGNUP_BTN = () => { return { type: "TOGGLE_LOGIN_SIGNUP_BTN" } }
 
+//      /LogInOutGoogle.tsx -------------->      const showform = (event:any) => {let targetid:string = event.target.id  TOGGLE_SHOW_FORM({payload: targetid}) }   ----> this also toggles state above.
 export const TOGGLE_SHOW_FORM = (action) => {
   return {
     type: "TOGGLE_SHOW_FORM",
@@ -16,25 +20,7 @@ export const TOGGLE_SHOW_FORM = (action) => {
   }
 }
 
-export const SET_PASSWORD_INPUT = (action) => {
-  return {
-    type: "SET_PASSWORD_INPUT",
-    payload: action.payload
-  }
-}
-
-export const SET_DUMMY_PASSWORD_INPUT = (action) => {
-  let actionCopy = action
-  const actionPayloadValue = actionCopy.payload
-  let payloadLength = actionPayloadValue.length
-  return {
-    type: "SET_DUMMY_PASSWORD_INPUT",
-    payload: action.payload
-    // payload: "*".repeat(payloadLength)
-    // payload: "*".repeat(action.payload.length)
-  }
-}
-
+//    /UsernameInput.tsx ------->    const usernameinputhandler = (event:any) => { inputHandler(event, SET_USERNAME_INPUT) }        ---------> utility/inputHandler SET_USERNAME_INPUT
 export const SET_USERNAME_INPUT = (action) => {
   return {
     type: "SET_USERNAME_INPUT",
@@ -42,6 +28,27 @@ export const SET_USERNAME_INPUT = (action) => {
   }
 }
 
+//    /PasswordInput.tsx ------->    const passwordinputhandler = (event:any) => { inputHandler(event, SET_PASSWORD_INPUT) }        ---------> utility/inputHandler SET_PASSWORD_INPUT 
+export const SET_PASSWORD_INPUT = (action) => {
+  return {
+    type: "SET_PASSWORD_INPUT",
+    payload: action.payload
+  }
+}
+
+
+//    /PasswordInput.tsx && [ issue #95 ]     forgot about <input type={"password"} this was a simple hasher for decoy state so that password would be hashed in GraphQL. DummyState was "hashed" client side to show on input.
+export const SET_DUMMY_PASSWORD_INPUT = (action) => {
+  let actionCopy = action
+  const actionPayloadValue = actionCopy.payload
+  let payloadLength = actionPayloadValue.length
+  return {
+    type: "SET_DUMMY_PASSWORD_INPUT",
+    payload: action.payload // payload: "*".repeat(action.payload.length)
+  }
+}
+
+//    /EmailInput.tsx ------->    const emailinputhandler = (event:any) => { inputHandler(event, SET_EMAIL_INPUT) }        ---------> utility/inputHandler SET_EMAIL_INPUT 
 export const SET_EMAIL_INPUT = (action) => {
   return {
     type: "SET_EMAIL_INPUT",
@@ -49,6 +56,7 @@ export const SET_EMAIL_INPUT = (action) => {
   }
 }
 
+//    /AgeInput.tsx ------->    const ageinputhandler = (event:any) => { inputHandler(event, SET_AGE_INPUT) }        ---------> utility/inputHandler SET_AGE_INPUT 
 export const SET_AGE_INPUT = (action) => {
   return {
     type: "SET_AGE_INPUT",
@@ -56,6 +64,7 @@ export const SET_AGE_INPUT = (action) => {
   }
 }
 
+//      /UsernameInput.tsx && /EmailInput.tsx && /PasswordInput.tsx && /AgeInput.tsx  -----> const inputfocus = { TOGGLE_INPUT_FOCUS( { payload: 'username'} ) } ---->  sets id "username|email" as react elem value={username}  
 export const TOGGLE_INPUT_FOCUS = (action) => { 
   return {  
     type: "TOGGLE_INPUT_FOCUS",
@@ -84,30 +93,22 @@ export const SET_ALL_EMAILS = (action) => {
   }
 }
 
-export const TOGGLE_PARENT_CONFIRM = (action) => {
-  return {
-    type: "TOGGLE_PARENT_CONFIRM"
-  }
-}
+//  /SignupLoginChecker.tsx  ------------>       if (loginstate === 'age') { } this checkbox, if the age is less than 10, allows user to use app -------------->    "/water_img/panda.png"
+export const TOGGLE_PARENT_CONFIRM = () => { return { type: "TOGGLE_PARENT_CONFIRM" } }
 
-export const TOGGLE_SUBMIT_INPUT_DATA = () => {
-  return {
-    type: "TOGGLE_SUBMIT_INPUT_DATA"
-  }
-}
+export const TOGGLE_SUBMIT_INPUT_DATA = () => { return { type: "TOGGLE_SUBMIT_INPUT_DATA" } }
 
-export const TOGGLE_GOOGLE_LINK_ACCT_SCREEN = () => {
-  return {
-    type: "TOGGLE_GOOGLE_LINK_ACCT_SCREEN"
-  }
-}
+export const TOGGLE_GOOGLE_LINK_ACCT_SCREEN = () => { return { type: "TOGGLE_GOOGLE_LINK_ACCT_SCREEN" } }
 
+//    /LogInOutGoogle.tsx ------------->        inputCheckingPromise    .then( () => { if (username_good === true && email_good === true && password_good === true && age_good === true) {      // SET_CURRENT_USER() 
 export const SET_CURRENT_USER = (action) => {
   return {
     type: "SET_CURRENT_USER",
     payload: action.payload
   }
 }
+
+export const TOGGLE_NO_LINK_GOOGLE_BTN_HOVER = () => { return { type: "NO_LINK_GOOGLE_BTN_HOVER "}}
 
 // export const SET_SIGNUP_INPUTS_COMPLETE = (action) => {
 //   return {
