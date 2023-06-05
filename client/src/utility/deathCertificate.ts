08c7bfb0803dd4cad313274f1e29a2c31c2fa16a
Frank Collins <fwc3rd@gmail.com>
2023-06-04 20:27:03 -0400

    [ fix issue #115 ] 🎳 [ fix issue #116 ]
    [8:26pm]

--- client/src/utility/deathCertificate.ts
+++ client/src/utility/deathCertificate.ts
@@ -1,7 +1,23 @@
-export default function deathCertificate (key:string, value:string, minutes:number) {
-        const item = {
-            value: value,
-            expirationDate: minutes
-        }
-        localStorage.setItem(key, JSON.stringify(item))
-}
+export default function deathCertificate(key: string, value: string, minutes: number) {
+    const actualMinutes: number = minutes * 60 * 1000;
+    const removeItem = () => localStorage.removeItem(key);
+    const item = {
+      value: value,
+      expirationDate: minutes,
+    };
+  
+    const deathPromise = new Promise<void>((resolve, reject,) => {
+      localStorage.setItem(key, JSON.stringify(item));
+      resolve(); // Resolve without any value since the operation is synchronous
+      // reject(console.log(localStorage.getItem(key))); // You might not need this rejection
+    });
+  
+    deathPromise.then(() => {
+      setTimeout(() => {
+        removeItem();
+      }, actualMinutes);
+    });
+  
+    return deathPromise;
+  }
+  
