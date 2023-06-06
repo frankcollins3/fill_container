75ec19220939dcdadfce7d4ad6d50bfb87f4f457
Frank Collins <fwc3rd@gmail.com>
2023-06-06 00:57:29 -0400

    loop over data, check type, perform whitespace removal if string [12:57am]

--- client/src/utility/fetch/userSignup.ts
+++ client/src/utility/fetch/userSignup.ts
@@ -1,13 +1,50 @@
-export default async function userSignup (user:any, NODE_ENV:string) {
-        // const myfetch = await fetch(`http://localhost:5000/fill_cont?query={userSignup{id,googleId,icon,username,email,age}}`)
-        let localURL:string = `http://localhost:5000/fill_cont?`
-        let query = `query={userSignup{id,googleId,icon,username,email,age}}`
-        if (NODE_ENV === 'development') {
-            const userSignup = await fetch(`${localURL}${query}`)
-            return userSignup
-        }
-        else if (NODE_ENV === 'production') {
-            // build URL and the same steps.
-        }
+import WaterRequest from '../WaterRequest'
+import RegexBank from '../RegexBank'
 
+export default async function userSignup (userObj:any, NODE_ENV:string) {     
+        const RegexMenu = await RegexBank()
+        let noWhiteSpace = RegexMenu.noWhiteSpace
+
+        // let localURL:string = `http://localhost:5000/fill_cont?`
+        const URLstring = NODE_ENV === 'development' ? `http://localhost:5000/fill_cont?` : 'amazonEC2/vercel.com/production'
+
+const { googleId, icon, username, email, password, age } = userObj  // ID to be set in /server/index.js GraphQL query with prisma.Users.findMany().length
+let userDataArray = [googleId, icon, username, email, password, age]
+
+        const whiteSpacePromise = new Promise( (resolve, reject) => {
+              userDataArray.forEach( (dataKey) => {
+                if (typeof dataKey === 'string') {
+                  dataKey.replace(noWhiteSpace, '')
+                }
+              })
+        })
+
+const newusername = username.replace(noWhiteSpace, '');
+
+const userSignupQuery = `query={userSignup(googleId:"G",icon:"I",username:"${newusername}",email:"you@you.you",password:"newpass",age:42){id,googleId,icon,username,email,password}}`;
+
+// const userSignupQuery = `query={userSignup(googleId:"G",icon:"I",username:"${newusername}",email:"${email}",password:"${password}",age:${age}){id,googleId,icon,username,email,password}}`;
+
+                                                              // cant have googleid or icon be empty!!
+// const userSignupQuery = `query={userSignup(googleId:"${googleId}",icon:"${icon}",username:"${username}",email:"${email}",password:"${password}",age:${age}){id,googleId,icon,username,email,password}}`;       
+            let query = `http://localhost:5000/fill_cont?query={userSignup{id,googleId,icon,username,email,password}}`
+
+            let headers = {
+                "Content-Type": "application/json",
+                Accept: "application/json",                
+            }
+    
+            let signupFetch = fetch(`${URLstring}${userSignupQuery}`, {
+                method: "POST",
+                headers: headers,
+                body: JSON.stringify({
+                  query,
+                //   args: { id, username },
+                }),
+              })
+            let data = (await signupFetch).json()
+            return data
+
+            // const url = `http://localhost:5000/fill_cont?query=${encodeURIComponent(userSignupQuery)}`;
+            // return userSignup
 }
