450ce488337563f72ef3fac91539e145446082e3
Frank Collins <fwc3rd@gmail.com>
2023-06-06 19:40:28 -0400

    link google with signed up user : add err handling [7:40pm]

--- client/src/utility/fetch/linkUserWithGoogle.ts
+++ client/src/utility/fetch/linkUserWithGoogle.ts
@@ -9,17 +9,23 @@ export default async function linkUserWithGoogle(signedUpUser:string|number, goo
     return urlPromise
     .then(async(urlbankdata:any) => {
         let env = urlbankdata.ENVdata.data.ENV
-        let NODE_ENV = env.NODE_ENV        
-        let username;
-        let id;
-        let miz:string = "mastermizery".replace(/\s/g, '')
+        let NODE_ENV = env.NODE_ENV                
         let googleId:string = '117827387775507687118'.replace(/\s/g, '')
         await localStorage.setItem("icon", icon)
+        // might make id handler but for now I only want to facilitate this data-fetching by accessing the username as params. not the ID
         let storageIcon = await localStorage.getItem("icon")    
         let href:string = NODE_ENV === 'development' ? `http://localhost:5000/` : 'www.EC2.amazon.deploy|vercel.prod/myapp'
-        const predata = await fetch(`http://localhost:5000/fill_cont?query={linkUserWithGoogle(username:"${encodeURIComponent(`${signedUpUser}`)}",icon:"${encodeURIComponent(`${storageIcon}`)}",googleId:"${encodeURIComponent(`${googleId}`)}"){id,googleId,icon,username,email,age}}`)   // W O R K S !!!!!
-        const data = await predata.json()        
-        return data        
+        try {
+            const predata = await fetch(`http://localhost:5000/fill_cont?query={linkUserWithGoogle(username:"${encodeURIComponent(`${signedUpUser}`)}",icon:"${encodeURIComponent(`${storageIcon}`)}",googleId:"${encodeURIComponent(`${googleId}`)}"){id,googleId,icon,username,email,age}}`)   // W O R K S !!!!!
+            const data = await predata.json()        
+            return data        
+        }
+        catch (err:any) {
+            const status:string = err.response.status
+            if (err.response && err.response.status === '401') { return `${status}` } 
+            return err
+        }
+        
     })
     // const test = async () => {
     //     let urlbank = await allUrl()
