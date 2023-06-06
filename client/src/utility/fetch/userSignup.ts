dc9c84dcaa527c43253e80374723c7b58b99974d
Frank Collins <fwc3rd@gmail.com>
2023-06-06 02:18:16 -0400

    [2:27am] fix issue #136 [2:17am]
    although issue #135 supposedly made sure there wouldn't be an issue #136 for that problem.

--- client/src/utility/fetch/userSignup.ts
+++ client/src/utility/fetch/userSignup.ts
@@ -13,6 +13,10 @@ export default async function userSignup(userObj: any, NODE_ENV: string) {
   const { googleId, icon, username, email, password, age } = userObj
   const userDataArray = [googleId, icon, username, email, password, age]
 
+  console.log('password from utility function')
+  console.log(password)
+  console.log(password.length)
+
   return new Promise((resolve, reject) => {
     const newdata = userDataArray.map((dataKey) => {
       if (typeof dataKey === 'string') {
@@ -22,28 +26,27 @@ export default async function userSignup(userObj: any, NODE_ENV: string) {
     })
 
     resolve(newdata)
-  })
-    .then(async (newdata: any) => {
-      const newusername = newdata[2]
+  }).then(async (newdata: any) => {
+    const newusername = newdata[2]
+    const email = newdata[3]
+    const newpassword = newdata[4]
+    const userSignupQuery = `query={userSignup(googleId:"G",icon:"I",username:"${newusername}",email:"${email}",password:"${encodeURIComponent(newpassword)}",age:${age}){id,googleId,icon,username,email,password}}`;
+    let query = `http://localhost:5000/fill_cont?query={userSignup{id,googleId,icon,username,email,password}}`
 
-      const userSignupQuery = `query={userSignup(googleId:"G",icon:"I",username:"${newusername}",email:"you@you.you",password:"newpass",age:42){id,googleId,icon,username,email,password}}`
+    let headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    }
 
-      let query = `http://localhost:5000/fill_cont?query={userSignup{id,googleId,icon,username,email,password}}`
-
-      let headers = {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      }
-
-      let signupFetch = fetch(`${URLstring}${userSignupQuery}`, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({
-          query,
-        }),
-      })
-
-      let data = await signupFetch
-      return data.json()
+    let signupFetch = fetch(`${URLstring}${userSignupQuery}`, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({
+        query,
+      }),
     })
+
+    let data = await signupFetch
+    return data.json()
+  })
 }
