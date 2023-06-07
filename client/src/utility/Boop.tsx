c7350124c4b4d3f7e4503479468e5b13ea05f86f
Frank Collins <fwc3rd@gmail.com>
2023-06-07 19:12:09 -0400

    .Boop() .animation() [7:12pm]

--- client/src/utility/Boop.tsx
+++ client/src/utility/Boop.tsx
@@ -1,66 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import $ from 'jquery'
 
 interface Props {
-  rotateAngle: number,
-  speed: number,
-  children: JSX.Element
+  rotateAngle: number;
+  speed: number;
+  children: JSX.Element;
 }
 
 const Boop = ({ rotateAngle, speed, children }: Props) => {
-  const [isRotated, setIsRotated] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   useEffect(() => {
-    let halfFassed:number = speed / 2 
-    if (isRotated) {
-      // Apply the rotation after a short delay to allow the transition to take effect
-      const timeoutId = setTimeout(() => {
-        const elements = document.getElementsByClassName('boop-child');
-        for (let i = 0; i < elements.length; i++) {
-          const element = elements[i] as HTMLElement;
-          element.style.transform = `rotate(${rotateAngle}deg)`;
-          setTimeout( () => element.style.transform = `rotate(-${rotateAngle}deg)`, halfFassed)
-          setTimeout( () => element.style.transform = `rotate(0)`, speed)
-          
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
 
-        }
-      }, 10);
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
 
-      return () => clearTimeout(timeoutId);
-    } else {
-      // Reset the rotation
-      const elements = document.getElementsByClassName('boop-child');
-      for (let i = 0; i < elements.length; i++) {
-        const element = elements[i] as HTMLElement;
-        element.style.transform = '';
-      }
+    const childElement = children.props.children;
+
+    childElement.addEventListener('mouseenter', handleMouseEnter);
+    childElement.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      childElement.removeEventListener('mouseenter', handleMouseEnter);
+      childElement.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [children]);
+
+  const getTransformStyle = () => {
+    if (isHovered) {
+      return `rotate(${rotateAngle}deg)`;
     }
-  }, [isRotated, rotateAngle]);
-
-  const boopBehaviorEnter = () => {
-    setIsRotated(true);
+    return '';
   };
 
-  const boopBehaviorLeave = () => {
-    setIsRotated(false);
-  };
-
-  return (
-    <div className="boop">
-      {React.Children.map(children, (child, index) => {
-        const styledChild = React.cloneElement(child, {
-          className: 'boop-child',
-          style: {
-            margin: 0
-          },
-          onMouseEnter: boopBehaviorEnter,
-          onMouseLeave: boopBehaviorLeave
-        });
-
-        return styledChild;
-      })}
-    </div>
-  );
+  return React.cloneElement(children, {
+    style: {
+      ...children.props.style,
+      transform: getTransformStyle(),
+      transition: `transform ${speed}ms`,
+    },
+  });
 };
 
 export default Boop;
