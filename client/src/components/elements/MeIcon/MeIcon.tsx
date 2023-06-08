837c5238448c47835d114ad380089ec7932e02f5
Frank Collins <fwc3rd@gmail.com>
2023-06-08 19:15:51 -0400

    {context} img src ready by render [7:15pm]
    no async functionality variables render right away 
    when <ContextProvider> wraps <App{components}>

--- client/src/components/elements/MeIcon/MeIcon.tsx
+++ client/src/components/elements/MeIcon/MeIcon.tsx
@@ -1,12 +1,160 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {useState, useEffect} from 'react'
+import "./meicon.css"
+import CSS from '../../../utility/CSS'
+import Boop from '../../../utility/ParentchildParent/Boop'
+import Boooooop from '../../../utility/ParentchildParent/Boooooop'
+import {useImage} from '../../../utility/ImgContext'
+// import LetterLife from '../../../utility/ParentchildParent/LetterLife'
+import $ from 'jquery'
 
-export default function MeIcon (props:any) {
+
+ function MeIcon (props:any) {
+
+    const { boat, pants, shark, panda, bikini, turtle, dolphin, pool, target, bucket  } = useImage()
+    
+    let img;
+
+    useEffect( () => {
+      (async() => {
+        
+
+      })()
+      
+    }, [])
+
+    const { FLIP_FLOP_ICON } = props
+    const boat2= `/water_img/`
+
+
+    const [bathingSuit, setBathingSuit] = useState<boolean>(false)
+
+
+    let googleUrl:string = props.GOOGLE_IMAGE_URL
+
+    const Leftie = ["Left-Cont", "Cont"].join(" ")
+    const Rightie = ["Right-Cont", "Cont"].join(" ")
+
+
+
+
+    const Boop = ({ children }: { children: JSX.Element }) => {
+        return (
+          <div className="boop">
+            {React.Children.map(children, (child, index) => {
+                const styledChild = React.cloneElement(child, {
+                    style: {
+                        border: '5px solid green'
+                    },
+
+                });
+                
+              return styledChild;
+            })}
+          </div>
+        );
+      }
+
+    const LetterLift= ({ children }: { children: JSX.Element }) => {
+        return (
+          <div className="boop">
+            {React.Children.map(children, (child, index) => {
+                const styledChild = React.cloneElement(child, {
+                    style: {
+                        border: '5px solid green'
+                    },
+
+                });
+                
+              return styledChild;
+            })}
+          </div>
+        );
+      }
+
+    // const LetterLift = ({ children}: { children: JSX.Element}) => {
+    //     return (
+    //         <div>
+    //             {React.Children.map(children, (child, index) => {
+    //                 const styledChild =React.cloneElement(child, {
+    //                     style; {
+    //                         border: '5px solid orange'
+    //                     },
+
+    //                 });
+    //                 return styledChild
+    //             })}
+    //         </div>
+    //     )
+    // }
+
+    const test = () => {
+        
+    }
+
+
     const renderMeIcon = () => {
         return (
-            <div>
-                <h1> hey its me again </h1>
-            </div>
+            <>
+              <div
+              className={Leftie}>
+                    <img style={{  height: '10em', width: '10em' }} src={ boat || '/water_img/mouse_droplet.png'}/>
+                    {/* <img style={{  height: '10em', width: '10em' }} src="/water_img/boat.png"/> */}
+                    
+                        
+              </div>  
+                    {/* if there is a current user and they hover over the icon it will become a window so one can see into the profile  */}
+              <div className={Rightie}>                        
+                    {/* <Boop rotateAngle={15} speed={500}> */}
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>
+                    <img onClick={test} style={{ transform: `rotate(0deg)` }} src="/water_img/bottles.png" />
+                    </Boooooop>
+                    {/* </Boop> */}
+
+                    {/* <Boop rotateAngle={15} speed={500}> */}
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                         
+                    <img onMouseEnter={()=> setBathingSuit(!bathingSuit)} src={bathingSuit ? bikini: pants}></img>
+                    </Boooooop>                         
+                    {/* </Boop>                 */}
+
+                    {/* <Boooooop rotateAngle={15} speed={500}>                         */}
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={shark}></img>
+                    {/* </Boooooop> */}
+                    </Boooooop>
+
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={panda}></img>
+                    </Boooooop>
+
+
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={turtle}></img>
+                    </Boooooop>
+
+
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={dolphin}></img>
+                    </Boooooop>
+
+
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={pool}></img>
+                    </Boooooop>
+                    
+                    
+                    
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={target}></img>
+                    </Boooooop>
+                    
+                    
+                    <Boooooop rotateAngle={45} speed={800} keepGoing={FLIP_FLOP_ICON}>                        
+                    <img src={bucket}></img>
+                    </Boooooop>                    
+              </div>  
+            </>
         )
     }
 
@@ -14,11 +162,11 @@ export default function MeIcon (props:any) {
 }
 
 const mapStateToProps = (state:any) => ({
-    ICON_NOT_INPUT: state.ICON_NOT_INPUT
+    ICON_NOT_INPUT: state.ICON_NOT_INPUT,
+    FLIP_FLOP_ICON: state.FLIP_FLOP_ICON
+    // NON_GOOGLE_IMG_URL: '',                      state doesn't matter since this page is navigated. one would need redux-persist. I'm using regular redux, GraphQl/prisma/postgres and localStorage to persist
 })
 
-const mapDispatchToProps = (dispatch:any) => ({
-    
-})
+const ConnectedMeIcon = connect(mapStateToProps)(MeIcon)
 
-const ConnectedMeIcon = connect(mapStateToProps, mapDispatchToProps)(MeIcon)
+export default ConnectedMeIcon
