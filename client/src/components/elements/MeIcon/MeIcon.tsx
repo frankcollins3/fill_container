8b0d0fe50f14d675ff5a13bf87aca9e59c04755a
Frank Collins <fwc3rd@gmail.com>
2023-06-09 18:29:52 -0400

    fix issue #171 -> :focus from onMouseEnter{} [6:29pm]

--- client/src/components/elements/MeIcon/MeIcon.tsx
+++ client/src/components/elements/MeIcon/MeIcon.tsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {useState, useEffect} from 'react'
 import "./meicon.css"
 import CSS from '../../../utility/CSS'
+import MathRandom from '../../../utility/MathRandom'
 import Boop from '../../../utility/ParentchildParent/Boop'
 import Boooooop from '../../../utility/ParentchildParent/Boooooop'
 import {useImage} from '../../../utility/ImgContext'
@@ -10,12 +11,12 @@ import {useImage} from '../../../utility/ImgContext'
 import $ from 'jquery'
 import ConnectedSignupLoginChecker from '../SignupLoginChecker'
 
-import { SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING,} from '../../../redux/actions'
+import { SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING, TOGGLE_SPIN_BOTTLE_SHOW_INPUT} from '../../../redux/actions'
 
 
  function MeIcon (props:any) {
 
-    const { boat, pants, shark, panda, bikini, turtle, dolphin, pool, target, bucket, puppetCup } = useImage()
+    const { boat, pants, shark, panda, bikini, turtle, dolphin, pool, target, bucket, puppetCup, cup, drink, bottle, bottles, mouseWaterCup, puppeteerSearchTerms } = useImage()
     
     let img;
 
@@ -28,18 +29,14 @@ import { SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING,} from '../../../redu
     }, [])
 
     const {
-       FLIP_FLOP_ICON, SPIN_BOTTLE_IMG, SPIN_BOTTLE_SEARCHING,
-       SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING,
+       FLIP_FLOP_ICON, SPIN_BOTTLE_IMG, SPIN_BOTTLE_SEARCHING, SPIN_BOTTLE_SHOW_INPUT,
+       SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING, TOGGLE_SPIN_BOTTLE_SHOW_INPUT,
      } = props
-
-    const boat2= `/water_img/`
 
 
     const [bathingSuit, setBathingSuit] = useState<boolean>(false)
     const [moveBoat, setMoveBoat] = useState(false)
     const [flipCoin, setFlipCoin] = useState(false)
-    const boatClassArr = ['Move-Boat', 'Move-BoatR']
-
 
     let googleUrl:string = props.GOOGLE_IMAGE_URL
 
@@ -87,18 +84,71 @@ import { SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING,} from '../../../redu
     const moveboat = () =>  {
       let numbers = [1,2,3,4,5,6,7,8,9,10]
       let randomNumber = Math.floor(Math.random() * numbers.length)
-      console.log('randomNumber')
-      console.log(randomNumber)
-      console.log('flipCoin')
-      console.log(flipCoin)
       if (randomNumber >= 5) {
         
         setFlipCoin(!flipCoin)
       }
       setMoveBoat(true) 
     }
-    
 
+    const TextInput = (event:any) => {
+        TOGGLE_SPIN_BOTTLE_SHOW_INPUT()
+      let value:string = `lightblue${event.target.value}`
+      console.log('value')
+      console.log(value)
+      
+      const loadingicons:string[] = [cup, drink, bottle, bottles, mouseWaterCup]
+          const randomIcon = MathRandom(loadingicons)
+          console.log('randomIcon')
+          console.log(randomIcon)  
+          const PuppetPromise = new Promise( (resolve, reject) => {
+            let terms:string[] = ["blue-ocean", "blue-water", "blue-river", "blue-seacreature", "blue-fish", "blue-octopus", "blue-shark", ]
+            let randomTerm = MathRandom(terms)
+            console.log(randomTerm)
+              TOGGLE_SPIN_BOTTLE_SEARCHING()
+              resolve(fetch(`http://localhost:5000/fill_cont?query={puppeteer(searchTerm:"light-blue-${value}")}`))    
+              // resolve(fetch(`http://localhost:5000/fill_cont?query={puppeteer(searchTerm:"${MathRandom(puppeteerSearchTerms)}")}`))    
+              reject(SET_SPIN_BOTTLE_IMG( { payload: randomIcon || '/water_img/squid.png' }))  
+          })
+          PuppetPromise.then(async (data:any) => {
+              if (data) {
+                data = await data.json()
+                console.log('data')
+                console.log(data)
+                let imgSrc:string = data.data.puppeteer
+                SET_SPIN_BOTTLE_IMG( { payload: imgSrc })
+                TOGGLE_SPIN_BOTTLE_SEARCHING()
+              } else {
+                TOGGLE_SPIN_BOTTLE_SEARCHING()
+                SET_SPIN_BOTTLE_IMG( { payload: "/water_img/bite.png"})
+              }
+          })
+        }
+
+        const FakeClick = (event:any) => { 
+          let key:string = event.key
+          console.log('key')
+          console.log(key)
+
+          let value:string = event.target.value
+          console.log('value')
+          console.log(value)
+
+          if (event.target.value === 'Enter') {
+            console.log(`enter us hitting: ${value}`)
+            event.target.click() 
+          }
+        }
+
+        const FakeFocus = (event:any) => {
+          let myElement = document.getElementById(event.target.id)
+          myElement !== null ? myElement.focus() : console.log(myElement)        
+        }
+        const iFocus = (event:any) => {
+          event.target.style.border = "1px dashed #73defe";
+        }
+
+  
     const dontMoveBoat = () =>  setMoveBoat(false) 
 
     const renderMeIcon = () => {
@@ -107,7 +157,17 @@ import { SET_SPIN_BOTTLE_IMG, TOGGLE_SPIN_BOTTLE_SEARCHING,} from '../../../redu
 <div style={{ backgroundImage: `url('${puppetCup}')` }} className={Leftie} onMouseEnter={moveboat} onMouseLeave={dontMoveBoat}>
 
 {/* <div style={{ height: '100%', width: '100%', backgroundImage: `url('${puppetCup}')`, backgroundRepeat: 'no-repeat' }}> */}
-                    <img className={ SPIN_BOTTLE_SEARCHING ? "Move-Boat" : '' } id="boat" style={{ position: 'relative', height: '10em', width: '10em' }} src={ boat || '/water_img/mouse_droplet.png'}/>
+                    {
+                          SPIN_BOTTLE_SHOW_INPUT 
+                          ?
+                          <div className="column">
+
+                          <input id="PuppeteerSearch" onFocus={iFocus} onChange={FakeClick} onMouseEnter={FakeFocus} style={{ color: 'blanchedalmond' }} type="text"/>
+                          </div>
+                          :
+                          <img className={ SPIN_BOTTLE_SEARCHING ? "Move-Boat" : '' } id="boat" style={{ position: 'relative', height: '10em', width: '10em' }} src={ boat || '/water_img/mouse_droplet.png'}/>
+                          
+                    }
 
               </div>  
                     {/* if there is a current user and they hover over the icon it will become a window so one can see into the profile  */}
@@ -212,13 +272,15 @@ const mapStateToProps = (state:any) => ({
     ICON_NOT_INPUT: state.ICON_NOT_INPUT,
     FLIP_FLOP_ICON: state.FLIP_FLOP_ICON,
     SPIN_BOTTLE_IMG: state.SPIN_BOTTLE_IMG,
+    SPIN_BOTTLE_SHOW_INPUT: state.SPIN_BOTTLE_SHOW_INPUT,
     SPIN_BOTTLE_SEARCHING: state.SPIN_BOTTLE_SEARCHING,
     // NON_GOOGLE_IMG_URL: '',                      state doesn't matter since this page is navigated. one would need redux-persist. I'm using regular redux, GraphQl/prisma/postgres and localStorage to persist
 })
 
 const mapDispatchToProps = (dispatch:any) => ({
     SET_SPIN_BOTTLE_IMG: (action:any) => dispatch(SET_SPIN_BOTTLE_IMG(action)),
-    TOGGLE_SPIN_BOTTLE_SEARCHING: () => dispatch(TOGGLE_SPIN_BOTTLE_SEARCHING())
+    TOGGLE_SPIN_BOTTLE_SEARCHING: () => dispatch(TOGGLE_SPIN_BOTTLE_SEARCHING()),
+    TOGGLE_SPIN_BOTTLE_SHOW_INPUT: () => dispatch(TOGGLE_SPIN_BOTTLE_SHOW_INPUT())
 })
 
 const ConnectedMeIcon = connect(mapStateToProps, mapDispatchToProps)(MeIcon)
