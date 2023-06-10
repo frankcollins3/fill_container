312c7ad5363b9a5618a9982f81aaf0a38f10e114
Frank Collins <fwc3rd@gmail.com>
2023-06-10 16:27:37 -0400

    fix issue #182, type:RegExp && centralize Contexts [4:26pm]

---
+++ client/src/utility/Contexts/RegexMenu.tsx
@@ -0,0 +1,71 @@
+import React, { createContext, useContext, ReactNode, useState } from "react";
+
+type RegexContextType = {
+    RstringAfterPeriod: RegExp;
+    RreturnLettersAthruZ: RegExp;
+    RreturnNumbers: RegExp;
+    RhasCaps: RegExp;
+    RhasNums: RegExp;
+    RnoWhiteSpace: RegExp;
+    MsplitAtDot: RegExp;
+  }
+  
+  type RegexDefaults = {
+    RstringAfterPeriod: RegExp;
+    RreturnLettersAthruZ: RegExp;
+    RreturnNumbers: RegExp;
+    RhasCaps: RegExp;
+    RhasNums: RegExp
+    RnoWhiteSpace: RegExp;
+    MsplitAtDot: RegExp;
+  }
+  
+  const regexDefaults: RegexDefaults = {
+    RstringAfterPeriod: /^.*\.(.*)$/,
+    RreturnLettersAthruZ: /[a\-z]/g,
+    RreturnNumbers: /[0\-9]/g,
+    RhasCaps: /[A-Z]/g,
+    RhasNums: /[A-Z]/g,
+    RnoWhiteSpace: /\s/g,
+    MsplitAtDot: /@([^.]*)\./        
+  };
+
+      const RegexContext = createContext<RegexContextType>(regexDefaults)
+      
+    export function useRegex() {
+        return useContext(RegexContext)
+    }
+
+    interface Props {
+        children: ReactNode
+    };
+
+    export function RegexProvider( { children } : Props ) {
+        const [RstringAfterPeriod, setStringAfterPeriod] = useState<RegExp>(/^.*\.(.*)$/)            // replace      
+        const [RreturnLettersAthruZ, setReturnLetterseAthruZ] = useState<RegExp>(/[a\-z]/g)          // replace
+        const [RreturnNumbers, setReturnNumbers] = useState<RegExp>(/[0\-9]/g)                       // replace
+        const [RhasCaps, setHasCaps] = useState<RegExp>(/[A-Z]/g)                                    // replace
+        const [RhasNums, setHasNums] = useState<RegExp>(/[A-Z]/g)                                    // replace
+        const [RnoWhiteSpace, setNoWhiteSpace] = useState<RegExp>(/\s/g)                             // replace
+        const [MsplitAtDot, setSplitAtDot] = useState<RegExp>(/@([^.]*)\./)      // match
+        
+        const value = {
+            RstringAfterPeriod,
+            RreturnLettersAthruZ,
+            RreturnNumbers,
+            RhasCaps,
+            RhasNums,
+            RnoWhiteSpace,
+            MsplitAtDot,       
+        };
+
+        return (
+            <RegexContext.Provider value={value}>
+                {children}
+            </RegexContext.Provider>
+        )
+    }
+
+
+        
+  
