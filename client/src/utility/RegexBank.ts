dd721633e1a8b866d62859b10165a4a2c5031318
Frank Collins <fwc3rd@gmail.com>
2023-06-10 15:17:36 -0400

    new regex endpoint for issue #181 [3:17pm]

--- client/src/utility/RegexBank.ts
+++ client/src/utility/RegexBank.ts
@@ -7,20 +7,7 @@ export default function RegexBank () {
         hasCaps: /[A-Z]/g,
         hasNums: /[A-Z]/g,
         noWhiteSpace: /\s/g,
+        splitAtDot: /@([^.]*)\./        
     }
-    return regexobject
-    // let regobj = regexobject
-
-    // if (regex === "stringAfterPeriod") {
-    //     return regobj.stringAfterPeriod        
-    // }
-    // if (regex === "returnNumbers") {
-    //     return regobj.returnNumbers
-    // }
+    return regexobject    
 }
-
-// const regexobject = {
-//     stringAfterPeriod: str.replace(/^.*\.(.*)$/, '$1'),
-//     returnLettersAthruZ: str.replace(/[a\-z]/g, ''),
-//     returnNumbers: str.replace(/[0\-9]/g, '')
-// }
