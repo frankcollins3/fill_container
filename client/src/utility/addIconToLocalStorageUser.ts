ba2a23862c95bcbc844a38f1601259dc700d765b
Frank Collins <fwc3rd@gmail.com>
2023-06-11 14:58:23 -0400

    automatically expires in 1 week [2:58pm]

--- client/src/utility/addIconToLocalStorageUser.ts
+++ client/src/utility/addIconToLocalStorageUser.ts
@@ -1,4 +1,5 @@
 import React from 'react'
+import deathCertificate from './deathCertificate'
 
 export default async function addIconToLocalStorageUser (icon:string) {
     const empty:string[]|undefined[] = [];
@@ -17,39 +18,12 @@ export default async function addIconToLocalStorageUser (icon:string) {
             const updatedUserToLocStorPromise = new Promise( (resolve:any, reject:any) => {
                 let clonedUser = {...user}
                 let userString:string = JSON.stringify(clonedUser)
-                localStorage.setItem("wateruser", userString)
-                let storageConfirmationToken = localStorage.getItem("wateruser") ? "WATER" : "H2NO"
+                // localStorage.setItem("wateruser", userString)
+                deathCertificate("wateruser", userString, 10080, true)
+                let storageConfirmationToken = localStorage.getItem("wateruser") ? "yes-user" : "no-user"
                 resolve(storageConfirmationToken)
                 reject(empty)
-            })            
+            })    
+            return updatedUserToLocStorPromise              // when documentation (like Prisma) says the functionality returns a Promise this is what returning a promise is. You get to use the .then block client-side.
         })
 }
-
-// const SaveUserHalf = async () => {                              
-//     const updateUserIconPromise = new Promise( (resolve:any, reject:any) => {
-//         let preUser = localStorage.getItem("user");              
-//         if (preUser !== null) {
-//           let userObj = JSON.parse(preUser);
-//           userObj.clone.data.userSignup.icon = NON_GOOGLE_IMG_URL
-//           resolve(userObj)
-//           reject(empty)
-//         }
-//     })
-//     updateUserIconPromise
-//     .then( (user:any) => {                
-//       const updatedUserToLocStorPromise = new Promise( (resolve:any, reject:any) => {
-//               let clonedUser = {...user}        
-//               let userString:string = JSON.stringify(clonedUser)                    
-//               localStorage.setItem("wateruser", userString)
-//               let storageConfirmationToken = localStorage.getItem("wateruser") ? "WATER" : ' '
-//               resolve(storageConfirmationToken)
-//               reject()
-//       })
-//       updatedUserToLocStorPromise
-//       .then( (wateruser:any) => {  
-//         TOGGLE_USER_ICON_CONFIRM()            
-//       })
-//     }).catch( (err:any) => {            
-//         return
-//     })              
-//   }
