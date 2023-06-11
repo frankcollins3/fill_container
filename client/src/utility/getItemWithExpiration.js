45110901743c4f843ccc96369595ba1a185b1cdf
Frank Collins <fwc3rd@gmail.com>
2023-06-11 17:18:40 -0400

    edge case to return img data instead of null [5:18pm]
    as this function facilitates retrieval of current_user thru locStor

--- client/src/utility/getItemWithExpiration.js
+++ client/src/utility/getItemWithExpiration.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function getItemWithExpiration(key) {
+export default function getItemWithExpiration(key) {
     if (!key) return null;
     
     const item = JSON.parse(localStorage.getItem(key));
     if (!item) return null;
     if (new Date().getTime() > item.expiration) {
       localStorage.removeItem(key); // Remove item if expired
-      return null;
+      return "/water_img/bite.png";
     }
     return item.value;
   }
