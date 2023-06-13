f3f37708fea111f795e05e7794cb53bc2ba7b0ad
Frank Collins <fwc3rd@gmail.com>
2023-06-13 19:08:16 -0400

    nothingFunc for onChange when onKeydown is used [7:08pm]

--- client/src/utility/UtilityValues.ts
+++ client/src/utility/UtilityValues.ts
@@ -1,2 +1,3 @@
 export const AgeArray = [1,2,3,4,5,6,7,8,9,10]
 export const ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+export const nothingFunc = () => { return }
