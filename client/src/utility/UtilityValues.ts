37fd3f0bfcfcf34b3eb97be214dca17141b21b10
Frank Collins <fwc3rd@gmail.com>
2023-06-14 04:44:53 -0400

    :number bucket for second arg of timeout/interval [4:44am]

--- client/src/utility/UtilityValues.ts
+++ client/src/utility/UtilityValues.ts
@@ -1,3 +1,9 @@
+import $ from 'jquery'
+
 export const AgeArray = [1,2,3,4,5,6,7,8,9,10]
 export const ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+export const intervalTimeouts = [1000,2000,3000,4000,5000]
+
 export const nothingFunc = () => { return }
+
+export const passwordTogglevalue = (state:any, setState:any) => { setState(!state) }
