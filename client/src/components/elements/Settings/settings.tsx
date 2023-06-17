c1e3e93a05867c1b12bef99479d37536bc608c70
Frank Collins <fwc3rd@gmail.com>
2023-06-16 22:29:35 -0400

    change local state to redux [10:29pm]

--- client/src/components/elements/Settings/settings.tsx
+++ client/src/components/elements/Settings/settings.tsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { connect, useDispatch } from 'react-redux'
-// import { useHistory } from 'react-router-dom';
-// import WAPPRequest from '../../utils';
+import {connect, useDispatch} from 'react-redux'
 import Spinner from '../Spinner';
 import './settings.css';
+import { SET_AGE, SET_WEIGHT, SET_HEIGHT, SET_START_TIME, SET_END_TIME, SET_REMINDER, SET_ACTIVITY, SET_UNITS, TOGGLE_LOADING } from '../../../redux/actions'
 
-export default function Settings(props) {
-  console.log('props')
-  console.log(props)
-  // const history = useHistory();
+interface Props {
+  AGE: number,
+  WEIGHT: number,
+  HEIGHT: number,
+  START_TIME: number,
+  END_TIME: number,
+  REMINDER: number,
+  ACTIVITY: number,
+  UNITS: string,
+  LOADING: number,
+
+  SET_AGE: any,
+  SET_WEIGHT: any,
+  SET_HEIGHT: any,
+  SET_START_TIME: any,
+  SET_END_TIME: any,
+  SET_REMINDER: any,
+  SET_ACTIVITY: any,
+  SET_UNITS: any,
+  TOGGLE_LOADING: any,
+}
+
+ function Settings(props: Props) {
+
+  const {
+    AGE, HEIGHT, WEIGHT, START_TIME, END_TIME, REMINDER, ACTIVITY, UNITS, LOADING,
+    SET_AGE, SET_WEIGHT, SET_HEIGHT, SET_START_TIME, SET_END_TIME, SET_REMINDER, SET_ACTIVITY, SET_UNITS, TOGGLE_LOADING
+  } = props
 
   const [age, setAge] = useState(0);
   const [weight, setWeight] = useState(0);
@@ -20,41 +43,35 @@ export default function Settings(props) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
 
-  useEffect(() => {
-    // const getUserSettings = async () => {
-    //   const response = await WAPPRequest('/profile/settings', {
-    //     method: 'GET',
-    //   }).catch((error) => console.log(error));
+  // useEffect(() => {
+  //   const getUserSettings = async () => {
+  //     const response = await WAPPRequest('/profile/settings', {
+  //       method: 'GET',
+  //     }).catch((error:any) => console.log(error));
 
-    //   if (response) {
+  //     if (response) {
 
-        // setWeight(150)
-        // setHeight(68)
-        // setStartTime(4)
-        // setReminder(8)
-        
+  //       setAge(Object.keys(response).length ? response.settings.age : 25);
+  //       setWeight(
+  //         Object.keys(response).length ? response.settings.weight : 150
+  //       );
+  //       setHeight(Object.keys(response).length ? response.settings.height : 65);
+  //       setStartTime(
+  //         Object.keys(response).length ? response.settings.startTime : 9
+  //       );
+  //       setEndTime(
+  //         Object.keys(response).length ? response.settings.endTime : 22
+  //       );
+  //       setReminder(
+  //         Object.keys(response).length ? response.settings.reminder : 1
+  //       );
+  //       setLoading(false);
+  //     }
+  //   };
+  //   getUserSettings();
+  // }, []);
 
-        // setAge(Object.keys(response).length ? response.settings.age : 25);
-        // setWeight(
-        //   Object.keys(response).length ? response.settings.weight : 150
-        // );
-        // setHeight(Object.keys(response).length ? response.settings.height : 65);
-        // setStartTime(
-        //   Object.keys(response).length ? response.settings.startTime : 9
-        // );
-        // setEndTime(
-        //   Object.keys(response).length ? response.settings.endTime : 22
-        // );
-        // setReminder(
-        //   Object.keys(response).length ? response.settings.reminder : 1
-        // );
-        // setLoading(false);
-      // }
-    // };
-    // getUserSettings();
-  }, []);
-
-  const renderDropdown = (num) => {
+  const renderDropdown = (num:any) => {
     const times = new Array(24).fill('');
     times[num] = true;
     return times.map((time, index) => (
@@ -62,7 +79,7 @@ export default function Settings(props) {
     ));
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e:any) => {
     e.preventDefault();
 
     const updatedSettings = {
@@ -81,22 +98,16 @@ export default function Settings(props) {
     // });
 
     // history.push('/');
-    window.location.reload();
+    // window.location.reload();
   };
 
   const handleReset = async () => {
-    console.log("guys were resetting things")
-  //   await WAPPRequest('/profile/reset', {
-  //     method: 'DELETE',
-  //     headers: { 'Content-Type': 'application/json' },
-  //   });
-  //   history.push('/');
-  //   window.location.reload();
+      console.log('hey thats a nice one')
   };
 
   const renderSettings = () => {
     if (loading) {
-      // history.push('/settings');
+
     }
     if (error) {
       return <div>Error</div>;
@@ -123,7 +134,7 @@ export default function Settings(props) {
         <div>{units.toUpperCase()}</div>
 
         <form onSubmit={handleSubmit}>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="age">
               <div>Age</div>
             </label>
@@ -133,12 +144,13 @@ export default function Settings(props) {
               name="age"
               min="0"
               max="100"
-              value={age}
-              onChange={(e) => setAge(parseInt(e.target.value))}
+              value={AGE}
+              onChange={(e:any) => SET_AGE({ payload: parseInt(e.target.value)})}
+              // onChange={(e:any) => setAge(parseInt(e.target.value))}
             />
-            <div>{age}</div>
+            <div>{AGE}</div>
           </div>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="weight">
               <div>Weight</div>
             </label>
@@ -157,7 +169,7 @@ export default function Settings(props) {
                 : `${weight} kg`}
             </div>
           </div>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="height">
               <div>Height</div>
             </label>
@@ -176,7 +188,7 @@ export default function Settings(props) {
                 : `${height} cm`}
             </div>
           </div>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="startTime">
               <div>What time do you start your day:</div>
             </label>
@@ -189,7 +201,7 @@ export default function Settings(props) {
               {renderDropdown(startTime)}
             </select>
           </div>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="endTime">
               <div>What time do you end your day:</div>
             </label>
@@ -204,7 +216,7 @@ export default function Settings(props) {
               {renderDropdown(endTime)}
             </select>
           </div>
-          <div className="form-group">
+          <div className="form-group-settings">
             <label htmlFor="reminder">
               <div> Notification Intensity</div>
             </label>
@@ -233,10 +245,31 @@ export default function Settings(props) {
   return <div className="settings-container">{renderSettings()}</div>;
 }
 
+
 const mapStateToProps = (state:any) => ({
-  
+    AGE: state.AGE,
+    WEIGHT: state.WEIGHT,
+    HEIGHT: state.HEIGHT,
+    START_TIME: state.START_TIME,
+    END_TIME: state.END_TIME,
+    REMINDER: state.REMINDER,
+    ACTIVITY: state.ACTIVITY,
+    UNITS: state.UNITS,
+    LOADING: state.LOADING
 })
 
-const mapDispatchToProps = (state) => ({
-
+const mapDispatchToProps = (dispatch:any) => ({
+    SET_AGE: (action:any) => dispatch(SET_AGE(action)),
+    SET_WEIGHT: (action:any) => dispatch(SET_WEIGHT(action)),
+    SET_HEIGHT: (action:any) => dispatch(SET_HEIGHT(action)),
+    SET_START_TIME: (action:any) => dispatch(SET_START_TIME(action)),
+    SET_END_TIME: (action:any) => dispatch(SET_END_TIME(action)),
+    SET_REMINDER: (action:any) => dispatch(SET_REMINDER(action)),
+    SET_ACTIVITY: (action:any) => dispatch(SET_ACTIVITY(action)),
+    SET_UNITS: (action:any) => dispatch(SET_UNITS(action)),
+    TOGGLE_LOADING: (action:any) => dispatch(TOGGLE_LOADING(action)),
 })
+
+const ConnectedSettings = connect(mapStateToProps, mapDispatchToProps)(Settings)
+
+export default ConnectedSettings
