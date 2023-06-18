bc3015197b914f49550fd6f69789af716a1a692b
Frank Collins <fwc3rd@gmail.com>
2023-06-18 19:33:57 -0400

    fix issue #229 2 properties in 1 string with [styleArr].join(" ") [7:33pm]

--- client/src/components/elements/Reminder/Reminder.tsx
+++ client/src/components/elements/Reminder/Reminder.tsx
@@ -1,57 +1,69 @@
 import React, { useState } from 'react';
 import { connect, useDispatch } from 'react-redux'
+import {useImage} from '../../../utility/Contexts/ImgContext'
 import './reminder.css';
 import $ from 'jquery'
 
+import { SET_PROGRESS, SET_STATUS_INDEX } from '../../../redux/actions'
   
 function Reminder(props: any) {
 
   const [isShown, setIsShown] = useState(false);
+  const reminderContJQ = $('#Reminder-Cont')[0]
 
-  const { time, amt, amtper, percent, index, status, disabled, HYDRO_SCHEDULE, HYDRO_DATA, HYDRO_INTAKE, STATUS, DISABLED } = props
+  const {mouseWaterCup, mouseDroplet} = useImage()
+
+  const { 
+    time, amt, amtper, percent, index, status, setStatus, disabled, HYDRO_SCHEDULE, HYDRO_DATA, HYDRO_INTAKE, STATUS, DISABLED,
+    SET_PROGRESS, SET_STATUS_INDEX
+  } = props
+
+  const transformers = ["rotate(90deg)", "scale(0.25)"].join(" ")
 
   //  time, amt, amtper, percent, index, status, disabled
 
   // const { amt, amtper, percent, index, status} = props
 
-  const handleClick = (event:any) => {
-    console.log('index')
-    console.log(index)
+  const handleClick = (index:number) => {
+    console.log(reminderContJQ)
 
-    console.log('well isnt that much easier')
-    console.log('HYDRO_INTAKE')
-    console.log(HYDRO_INTAKE)
+    const newStatus = [...status];
+    newStatus[index] = 'check';
+    setStatus(newStatus);
 
-    console.log('HYDRO_SCHEDULE.length')
-    console.log(HYDRO_SCHEDULE.length)
-
-    let calc = Math.floor((HYDRO_INTAKE / HYDRO_SCHEDULE.length) * (index + 1) / HYDRO_INTAKE)
+    // SET_STATUS( {payload: STATUS[index] === 'check'})
+    let calc = Math.ceil((HYDRO_INTAKE / HYDRO_SCHEDULE.length) * (index + 1) / HYDRO_INTAKE)
+    SET_PROGRESS( {payload: percent / 100 } )
+    // SET_PROGRESS( {payload: 100 / HYDRO_SCHEDULE.length / 100 } )
     console.log('percent')
-    console.log(percent)
-    
+    console.log(percent)    
     // console.log(Math.floor(hydroIntake / hydroSchedule.length) * (index + 1)) / hydroIntake))      
 }
 
 
   // reminder component
   return (
-    <>
+    <div id="Reminder-Cont">
+
       <li key={index}>
         <button
           // disabled={disabled[index]}
           className="btn"
-          onClick={handleClick}
+          onClick={() => handleClick(index)}
+          // onClick={handleClick}
           style={{
             backgroundColor: `#98ddfc${percent}`,
             width: `${percent}%`,
+            // width: `${percent}%`,
           }}
           onMouseEnter={() => setIsShown(true)}
           onMouseLeave={() => setIsShown(false)}
-        >
+          >
           <span>
             <span>
-              {/* { amtper.toFixed(2)} /{' '} {amt.toFixed(0)} fl oz */}
-              {status[index] === 'check' ? '✔DONE!⭐' : amtper.toFixed(2)} /{' '} {amt.toFixed(0)} fl oz
+              {status[index] === 'check' ? '' : amtper.toFixed(2)} /{' '} {amt.toFixed(0)} fl oz
+              { status[index] === 'check' ? <img style={{ transform: transformers }} src={mouseDroplet}/> : <pre></pre>}
+              {/* {status[index] === 'check' ? '✔DONE!⭐' : amtper.toFixed(2)} /{' '} {amt.toFixed(0)} fl oz */}
             </span>
           </span>
         </button>
@@ -59,14 +71,14 @@ function Reminder(props: any) {
           <span
             className="timeline-display"
             style={{ color:  '#00000030'  }} // style={{ color: `${disabled[index] ? '#00000030' : '#000000'}` }}
-          >
+            >
             {time <= 12 ? time : time - 12} {time < 12 ? 'am' : 'pm'}
           </span>
 
           {isShown && <div className="timeline-hover"><img src="/water_img/mouse_droplet.png" /></div>}
         </div>
       </li>
-    </>
+      </div>    
   );
 }
 
@@ -79,7 +91,12 @@ const mapStateToProps = (state:any) => ({
     DISABLED: state.DISABLED,
 })
 
-const ConnectedReminder = connect(mapStateToProps)(Reminder)
+const mapDispatchToProps = (dispatch:any) => ({
+    SET_PROGRESS: (action:any) => dispatch(SET_PROGRESS(action)), 
+    SET_STATUS_INDEX: (action:any) => dispatch(SET_STATUS_INDEX(action)), 
+})
+
+const ConnectedReminder = connect(mapStateToProps, mapDispatchToProps)(Reminder)
 export default ConnectedReminder
 
 
