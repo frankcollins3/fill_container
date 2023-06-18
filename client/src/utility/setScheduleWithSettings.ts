0d88c2f95c5146ffe56467e73b1d128c090e822d
Frank Collins <fwc3rd@gmail.com>
2023-06-18 16:40:04 -0400

    needs reminder for last block of time [4:40pm]
    2nd arg to loop cant suffice to < END_TIME length
    it must include that END_TIME as well.

--- client/src/utility/setScheduleWithSettings.ts
+++ client/src/utility/setScheduleWithSettings.ts
@@ -1,10 +1,13 @@
 export default async function setScheduleWithSettings (settings:any, setState:any) {
+    console.log('settings from schedule')
+    console.log(settings)
     let schedule:number[] = [];
     if (!settings) {
         return
     } else if (settings) {              
-      const SchedulePromise = new Promise((resolve:any, reject:any) => {
-        for (let i = settings.start_time; i < settings.end_time; i += settings.reminder) { schedule.push(i)}
+      const SchedulePromise = new Promise(async(resolve:any, reject:any) => {
+        const setSchedule = () => { for (let i = settings.start_time; i <= settings.end_time; i += settings.reminder) { schedule.push(i)} }
+        await setSchedule()
         resolve(schedule.length ? schedule : "schedule fail")
       })
       return SchedulePromise
