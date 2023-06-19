7f50daad1dc262fff526bcf6c4998560071f5912
Frank Collins <fwc3rd@gmail.com>
2023-06-18 23:44:05 -0400

    if user misses first cycle water drop becomes empty [11:43pm]

--- client/src/components/elements/Credits/Credits.tsx
+++ client/src/components/elements/Credits/Credits.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './credits.css';
 import { connect, useDispatch } from 'react-redux'
+import {useImage} from '../../../utility/Contexts/ImgContext'
+import ConnectedDisplay from '../../../components/elements/Display'
 
 import {TOGGLE_FLIP_FLOP_ICON} from "../../../redux/actions"
 
   function Credits( props: any ) {
 
+    const { PROGRESS, REMINDER_CLICK_COUNT } = props
+
+    const { bottle, smallDroplet, bg } = useImage()
+
   const {
     FLIP_FLOP_ICON, ICON_NOT_INPUT,
     TOGGLE_FLIP_FLOP_ICON
   } = props
-
-  console.log(FLIP_FLOP_ICON)
-  console.log(ICON_NOT_INPUT)
-
+  
   const stateToggle = () => {
     TOGGLE_FLIP_FLOP_ICON()
   }
@@ -23,27 +26,36 @@ import {TOGGLE_FLIP_FLOP_ICON} from "../../../redux/actions"
     return (
       <>
       <div>          
-            <img src="water_img/bottle.png" />          
+            <img src={bottle} />          
         </div>
   
         <div>
           {/* <a href="https://github.com/WAPP-Water-App">
           </a> */}
             
-            {
+            {/* {
           window.location.pathname === "/loginoutgoogle" && ICON_NOT_INPUT ? (
             <img style={ { opacity: FLIP_FLOP_ICON ? "1.0" : "0.333"}}
             onClick={stateToggle} src="/water_img/sandals.png" />
-          ) : (
-            <div></div>
-          )
-           }
-
+            ) : (
+              <div></div>
+              )
+            } */}
 
         </div>
         <div>
           <ul>
-            <img src="water_img/small_droplet.png"/>
+            {
+              PROGRESS > .01
+                    ?
+              <ConnectedDisplay/>
+                    :
+                REMINDER_CLICK_COUNT > 0 && PROGRESS < 0.1 
+                ? <img src={bg}/>
+                : <img src={smallDroplet}/>
+
+
+            }
   
           </ul>
         </div>  
@@ -59,7 +71,9 @@ import {TOGGLE_FLIP_FLOP_ICON} from "../../../redux/actions"
 
   const mapStateToProps = (state:any) => ({
     FLIP_FLOP_ICON: state.FLIP_FLOP_ICON,
-    ICON_NOT_INPUT: state.ICON_NOT_INPUT
+    ICON_NOT_INPUT: state.ICON_NOT_INPUT,
+    PROGRESS: state.PROGRESS,
+    REMINDER_CLICK_COUNT: state.REMINDER_CLICK_COUNT
 })
 
   const mapDispatchToProps = (dispatch:any) => ({
