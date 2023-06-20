2f412da0271a083229a2399d1a262a64eb605c35
Frank Collins <fwc3rd@gmail.com>
2023-06-20 15:35:58 -0400

    if user hasn't filled up, and timer < 45 mins. text = red [3:35pm]

--- client/src/components/elements/Timer/Timer.jsx
+++ client/src/components/elements/Timer/Timer.jsx
@@ -1,96 +1,170 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './timer.css';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment-timezone';
+import Calendar from 'react-calendar';
+import './dashboard.css';
+import 'react-calendar/dist/Calendar.css';
 
-// keeps track of the callback (/something)
-// "keeps the class var in line" -  if the callback ever changes
-// does not cause any re-rendering
-// the useeffect has a function tick that calls the current class variable
-// then tick is called with the setInterval function
-// the delay on the setInterval is passed through the useInterval
-// every time the delay value changes, change the setInterval
-// the return value from useffect can clean up after last useeffect
+import currentuserJSONparse from '../../../utility/currentuserJSONparse'
+import getAllUserData from '../../../utility/fetch/getAllUserData'
+import {useImage} from '../../../utility/Contexts/ImgContext'
+import {useRegex} from '../../../utility/Contexts/RegexMenu'
+import CSS from '../../../utility/CSS'
+import {currentDayBorderOn} from '../../../utility/UtilityValues'
+import {currentDayBorderOff} from '../../../utility/UtilityValues'
+import {connect, useDispatch} from 'react-redux'
+import { set } from 'date-fns';
+import Spinner from '../Spinner';
+import $ from 'jquery';
 
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-  // Remember the latest callback.
+import { TOGGLE_CALENDAR_DAY_DRIED_UP } from "../../../redux/actions"
+
+
+
+ function Dashboard({ CALENDAR_DAY_DRIED_UP, TOGGLE_CALENDAR_DAY_DRIED_UP }) {
+  const [value, onChange] = useState(new Date());
+  const [hydroData, setHydroData] = useState();
+  const [hydroDays, setHydroDays] = useState([]);
+  const [selectedDay, setSelectedDay] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [currentDay, setCurrentDay] = useState();
+  $('*').css('cursor', `url('/water_img/mouse_droplet.png'), auto`)
+  $('*').on('mouseenter', (event) => {
+    $(event.target).css('cursor', `normal`)
+  })
+
+  const tilenow = $('.react-calendar__tile--now')
+
+
+  const { close, confirmation } = useImage()
+  const { dayofMonthSplit } = useRegex()
+  
+
   useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
+    (async() => {
+       let currentUser = await currentuserJSONparse()
+       let currentuserID = currentUser.id
+
+       let currentUserDataArray = await getAllUserData(currentuserID)
+       let dataLength = currentUserDataArray.length
+      
+       const datePROMISE = new Promise( (resolve, reject) => {
+          setHydroData(currentUserDataArray);
+          const getDates = currentUserDataArray.map((day, index) => {            
+            const formattedDate = moment(day.date, 'YYYY-M-D').format('MM-DD-YYYY');
+            return formattedDate;
+          });
+          resolve(getDates ? getDates : "no dates")          
+        })
+        datePROMISE
+        .then( (actualdates) => {
+            console.log('actualdates')
+            console.log(actualdates)
+        })              
+
+
+    })()
+ 
+   }, []);
+
+
+  function getDataForClickedDay(highlightedDay) {
+    const formatHighlightDay = moment(highlightedDay, 'YYYY-M-D').format('MM-DD-YYYY')      
+    const today = new Date().getDate()  
+    const todaySlicer = formatHighlightDay.slice(3, 5)
+  
+    if (today.toString() === todaySlicer)  currentDayBorderOn() 
+    else if (today.toString() !== todaySlicer) currentDayBorderOff()
+
+
+    const getDay = hydroData.find(
+      (day) =>
+        moment(day.date, 'YYYY-M-D').format('MM-DD-YYYY') === formatHighlightDay
+        // moment(highlightedDay, 'YYYY-M-D').format('MM-DD-YYYY')
+    );
+    if (getDay) {
+      setSelectedDay(getDay);
+    } else {
+      setSelectedDay(null)
+      if (CALENDAR_DAY_DRIED_UP === false) TOGGLE_CALENDAR_DAY_DRIED_UP()
     }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
+  }
+
+  function tileClassName({ date, view }) {
+    // Add class to tiles in month view only
+    if (view === 'month') {
+      // Check if a date React-Calendar wants to check is on the list of dates to add class to
+      const formatDate = moment(date, 'YYYY-M-D').format('MM-DD-YYYY');
+      if (hydroDays.find((dDate) => formatDate === dDate)) {
+        return 'calendarHighlight';
+      }
     }
-  }, [delay]);
-}
+  }
 
-export default function Timer({ hydroSchedule, timer, setTimer }) {
-
-  useEffect(() => {
-    const currentTime = new Date().getTime();
-    const currentHour = new Date().getHours();
-
-    // find the closest matching time
-
-    const remainingReminders = hydroSchedule.filter((time) => {
-      return time > currentHour;
-    });
-
-    if (remainingReminders.length) {
-      const nextHour = new Date();
-      nextHour.setMinutes(0);
-      nextHour.setSeconds(0);
-
-      const timeToNextRemind = Math.ceil(
-        (nextHour.setHours(remainingReminders[0]) - currentTime) / 1000
-      );
-
-      setTimer(timeToNextRemind);
+  const renderDashboard = () => {
+    if (loading) {
+    // return <Spinner />;
     }
-  }, [hydroSchedule]);
 
-  useInterval(() => {
-    if (timer !== undefined) {
-      setTimer(timer - 1);
+    if (error) {
+      return <div>Error</div>;
     }
-  }, 1000);
 
-  const renderTimer = () => {
-    // right way to do the timer within an hour or so:          I dont want this I want the timer to be timing no matter what.
-
-    // if (timer < 0 || timer > 3600 * 6 || timer === NaN) {
-    //   return (
-    //     <div>
-    //     </div>
-    //   );
-    // } else if (!timer) {
-    //   return (
-    //     <div>
-    //       0:00
-    //     </div>
-    //   )
-    // }
     return (
-      <div>
-        {Math.floor(timer / 60).toString().length < 2
-          ? `0${Math.floor(timer / 60).toString()}`
-          : Math.floor(timer / 60)}
-        :
-        {(timer % 60).toString().length < 2
-          ? `0${(timer % 60).toString()}`
-          : (timer % 60).toString()}
-      </div>
+      <>
+        <div className="calendar-details">
+          {selectedDay ? (
+            <>
+              <div style={{ color: 'silver', fontWeight: 'bolder'}} className="calendar-date">
+                  {moment(selectedDay.date, 'YYYY-MM-DD').format('ddd, MMM D YYYY')}
+                  {/* {moment(selectedDay.date, 'YYYY-MM-DD').format('ddd, MMM D YYYY')} */}
+                {/* {moment(selectedDay.date,).format('ddd, MMM-DD-YY')} */}
+              </div>
+              <div className="calendar-progress">
+                Progress: {Math.ceil(selectedDay.progress)}%
+              </div>
+              <div className="calendar-status">
+                {selectedDay.status.map((d, index) => (
+                  <div key={`div${index}`}>
+                    {d === 'check' ? (     // jenny smart: keep non set variables false because they trigger ternary to falsy block. Problem is you can't type the state in an interface. toggle <booleanFalse> to <truthyString'Check'>
+                      <img key={`confirm${index}`} src={confirmation} />
+                    ) : (
+                      <img key={`close${index}`} src={close} />
+                    )}
+                  </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            // loading bar 
+            <div style={{ color: 'silver', fontWeight: 'bolder'}} className="calendar-progress"> {CALENDAR_DAY_DRIED_UP ? "All Dried Up" : "Pick A Highlighted Day"} </div>            
+            // <div className="calendar-progress">Pick A Highlighted Day</div>
+          )}
+        </div>
+        <div className="calendar">
+          <Calendar
+            tileClassName={tileClassName}
+            // tileClassName={tileClassName}
+            onChange={onChange}
+            value={value}
+            onClickDay={(value, event) => getDataForClickedDay(value)}
+          />
+        </div>
+      </>
     );
   };
 
-  return (
-    <div style={{ color: 'silver', fontWeight: 'bolder'}} className="timer">
-      Next Reminder:
-      {renderTimer()}
-    </div>
-  );
+  return <div className="dashboard-container">{renderDashboard()}</div>;
 }
+
+
+const mapStateToProps = (state) => ({
+    CALENDAR_DAY_DRIED_UP: state.CALENDAR_DAY_DRIED_UP
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    TOGGLE_CALENDAR_DAY_DRIED_UP: () => dispatch(TOGGLE_CALENDAR_DAY_DRIED_UP())
+})
+
+const ConnectedDashboard = connect(mapStateToProps, mapDispatchToProps)(Dashboard)
+export default ConnectedDashboard
