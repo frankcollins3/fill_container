2b0e2a519550502dcad553df7a02887ea52d6f0f
Frank Collins <fwc3rd@gmail.com>
2023-06-20 01:38:28 -0400

    fix issue #236 infinite loop state toggle [1:38am]

--- client/src/components/elements/Streak/Streak.jsx
+++ client/src/components/elements/Streak/Streak.jsx
@@ -1,51 +1,71 @@
 import { render } from '@testing-library/react';
 import React, { useState, useEffect } from 'react';
-import WAPPRequest from '../../utils';
+// import WAPPRequest from '../../utils';
 import Spinner from '../Spinner';
 import StreakDay from '../StreakDay/StreakDay';
+import currentuserJSONparse from '../../../utility/currentuserJSONparse'
+import {useImage} from '../../../utility/Contexts/ImgContext'
+import getAllUserData from '../../../utility/fetch/getAllUserData'
 import './streak.css';
 
 export default function Streak() {
   const [streakData, setStreakData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
+  const [localContHover, setLocalContHover] = useState(false)
+  const { mouseDroplet, puppetCup } = useImage()
 
   useEffect(() => {
-    const getAllDays = async () => {
-      const response = await WAPPRequest('/data/alldays', {
-        method: 'GET',
-      }).catch(() => {
-        setError(true);
-        return null;
-      });
+   (async() => {
+      let currentUser = await currentuserJSONparse()
+      let currentuserID = currentUser.id
+      let currentUserDataArray = await getAllUserData(currentuserID)
+      let dataLength = currentUserDataArray.length
+      console.log('currentUserDataArray')
+      console.log(currentUserDataArray)
 
-      if (response) {
-        let start = response.length < 7 ? 0 : response.length - 7;
-        setStreakData(response.slice(start, response.length));
-      }
-      setLoading(false);
-    };
+      if (currentUserDataArray) {
+        let start = dataLength < 7 ? 0 : dataLength - 7
+        setStreakData(currentUserDataArray.slice(start, dataLength))
+      } else { return }
+   })()
 
-    getAllDays();
   }, []);
 
   const renderStreak = () => {
     if (loading) {
-      return <Spinner />;
+      // return <Spinner />;
     }
     if (error) {
       return <div>Error</div>;
     }
     return streakData.map((day, index) => (
       <li key={index}>
-        <StreakDay data={day} />
+        <img style={{ margin: '0 0.5em'}} src={mouseDroplet}/>
+        {/* <StreakDay data={day} /> */}
       </li>
     ));
   };
 
+  const renderStreakExplain = () => {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
+        <img style={{ height: '35px', width: '35px'}} src={puppetCup}/>
+        <h6 style={{ color: 'silver', fontWeight: 'bolder', }}> Sip Streak </h6>
+      </div>
+        )
+  }
+
+  const contHoverFunc = () => {
+      setTimeout( () => setLocalContHover(true), 1000)
+      setTimeout( () => setLocalContHover(false), 3000)
+  }
+
   return (
     <div className="streak-container">
-      <ul>{streakData.length > 0 && <>{renderStreak()}</>}</ul>
+        {
+    localContHover === false ? <ul onMouseLeave={contHoverFunc}>{streakData.length > 0 && <>{renderStreak()}</>} </ul> : <ul>{streakData.length > 0 && <>{renderStreakExplain()}</>} </ul> 
+        }          
     </div>
   );
 }
