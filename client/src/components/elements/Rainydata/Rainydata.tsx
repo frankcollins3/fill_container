dc74741a170627a5ea04e6176eacde74a4c0af31
Frank Collins <fwc3rd@gmail.com>
2023-06-28 10:17:49 -0400

    :focus prop() clears val for user/human convenience [10:17am]

--- client/src/components/elements/Rainydata/Rainydata.tsx
+++ client/src/components/elements/Rainydata/Rainydata.tsx
@@ -1,20 +1,50 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 import {useImage} from '../../../utility/Contexts/ImgContext'
 import "./rainydata.css"
+import {nothingFunc} from '../../../utility/UtilityValues'
+import $ from 'jquery'
 
 const RainyData = () => {
+     
+    const [peek, setPeek] = useState(false);
+    const { window, curtain } = useImage();
+    const [inputVal, setInputVal] = useState("city name")
 
-    const { window } = useImage();
+    const peeek = () => setPeek(!peek)
+
+    const pullCurtain = () => {
+        if (!peek) {
+          console.log("no water peek")
+          peeek()
+        } else {
+          peeek()
+        }
+    }
+
+    const fakeChanger = (event:any) => {
+        let key:string = event.key
+        let value:string = event.target.value
+        console.log('key')
+        console.log(key)
+
+        console.log('value')
+        console.log(value)
+
+    }
+
+    const noValueFocus = () => { setInputVal('') }
 
     const RenderRainyData = () => {
         return (
             <>
-            <img src={window} />
-            <h6> Is it Raining Out There ? </h6>
+            <img onClick={pullCurtain} className="curtain" src={peek ? window : curtain} />
+            <h1 className="text"> { peek ? "Which City" : "Is it Raining Out there?" } </h1>
+            <input onKeyDown={fakeChanger} onChange={nothingFunc} onFocus={noValueFocus} type="text" id="input-val" value={inputVal}/>
+            {/* <h1 className="text"> Is it Raining Out There ? </h1> */}
             </>
         )
     }
-    return <div className="rainydata-cont"> {RenderRainyData()} </div>
+    return <div id="rainydata-cont"> {RenderRainyData()} </div>
 
 }
 
