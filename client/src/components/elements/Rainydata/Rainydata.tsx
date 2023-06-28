9aa559a9a29647ce44b22691227ed64c1a07233a
Frank Collins <fwc3rd@gmail.com>
2023-06-28 11:27:00 -0400

    fix issue #246 keydown backspace [11:26am]

--- client/src/components/elements/Rainydata/Rainydata.tsx
+++ client/src/components/elements/Rainydata/Rainydata.tsx
@@ -9,6 +9,8 @@ const RainyData = () => {
     const [peek, setPeek] = useState(false);
     const { window, curtain } = useImage();
     const [inputVal, setInputVal] = useState("city name")
+    const [focusYet, setFocusYet] = useState(false)
+    const [weatherCity, setWeatherCity] = useState("")
 
     const peeek = () => setPeek(!peek)
 
@@ -24,22 +26,30 @@ const RainyData = () => {
     const fakeChanger = (event:any) => {
         let key:string = event.key
         let value:string = event.target.value
+        const valueLength:number = inputVal.length        
         console.log('key')
         console.log(key)
-
-        console.log('value')
-        console.log(value)
+        const clickCondition = key === 'Backspace' || key === 'Tab' || key === 'Meta' || key === 'Control' || key === 'Shift' || key === 'Alt' || key === 'Option' || key === 'Enter'
+        if (clickCondition) {
+            console.log('Meta or friends')
+            return
+        } else { key === "Backspace" ? setInputVal(`${inputVal.slice(0, -1)}`) : setInputVal(`${inputVal}${key}`) }
 
     }
 
-    const noValueFocus = () => { setInputVal('') }
+    
+
+
+    const noValueFocus = () => { focusYet ? nothingFunc() : setInputVal('') }
 
     const RenderRainyData = () => {
         return (
             <>
             <img onClick={pullCurtain} className="curtain" src={peek ? window : curtain} />
             <h1 className="text"> { peek ? "Which City" : "Is it Raining Out there?" } </h1>
-            <input onKeyDown={fakeChanger} onChange={nothingFunc} onFocus={noValueFocus} type="text" id="input-val" value={inputVal}/>
+            <input style={{ display: peek ? "" : "none" }} onKeyDown={fakeChanger} onChange={nothingFunc} onFocus={noValueFocus} type="text" id="input-val" value={inputVal}/>
+            {/* <input style={{ display: peek ? "" : "none" }}  onChange={changeHandler} onFocus={noValueFocus} type="text" id="input-val" value={inputVal}/> */}
+
             {/* <h1 className="text"> Is it Raining Out There ? </h1> */}
             </>
         )
