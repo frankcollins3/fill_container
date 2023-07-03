70691d2df7ac5c6974d1b20a84ada6c8b9e28879
Frank Collins <fwc3rd@gmail.com>
2023-07-03 01:30:31 -0400

    fix issue #256 ty chatGPT return 10-20hrs of life to me [1:26am]
    webpack allows for ultra specific config?
    or webpack requires ultra specific config.
    
    npm i puppeteer: child_process (@puppeteer/browsers) 
    
    back of mind saving a reddit comment/opinion:
    "easier to migrate to vite.js than it is to upgrade webpack"

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './server/index.js',
@@ -9,5 +10,25 @@ module.exports = {
   },
   plugins: [
     new Dotenv(),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^child_process$/,
+    }),
   ],
+  resolve: {
+    fallback: {
+      fs: false, // Disable resolving 'fs' module
+      stream: require.resolve('stream-browserify'), // Resolve 'stream' module using 'stream-browserify' package
+      https: require.resolve('https-browserify'),
+      http: require.resolve('stream-http'),
+      querystring: require.resolve('querystring-es3'), // Add fallback for 'querystring' module
+      crypto: require.resolve('crypto-browserify'), // Add fallback for 'crypto' module
+      constants: require.resolve('constants-browserify'), // Add fallback for 'constants' module
+      module: require.resolve('module'), // Add fallback for 'module' module
+      url: require.resolve('url/'),
+      zlib: require.resolve('browserify-zlib'),
+      os: require.resolve('os-browserify/browser'),
+      assert: require.resolve('assert/'),
+      './zlib_bindings': require.resolve('browserify-zlib/lib/binding'),
+    },
+  },
 };
